bee1ef27854ebb25ade2063b132d3d2731ac68b0
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-01-27 21:51:55 +0000

    Style

--- routes/routes.php
+++ routes/routes.php
@@ -15,7 +15,7 @@ $router->group(['middleware' => 'scopes:' . AuthServiceProvider::SCOPE_USER], fu
             'uses' => 'TeapotController@teapot',
         ]
     );
-    
+
     // Version checking
     $router->get(
         'version/{version:[A-Za-z0-9\.\-]+}/status',
@@ -26,15 +26,15 @@ $router->group(['middleware' => 'scopes:' . AuthServiceProvider::SCOPE_USER], fu
             'uses' => 'VersionController@getVersionStatus'
         ]
     );
-    
+
     // Dependencies
     $router->get('dependency', 'DependencyController@getManifest');
-    
+
     // Squawks
     $router->get('squawk-assignment/{callsign:[A-Za-z0-9\-]{1,10}}', 'SquawkController@getSquawkAssignment');
     $router->put('squawk-assignment/{callsign:[A-Za-z0-9\-]{1,10}}', 'SquawkController@assignSquawk');
     $router->delete('squawk-assignment/{callsign:[A-Za-z0-9\-]{1,10}}', 'SquawkController@deleteSquawkAssignment');
-    
+
     // Regional Pressure
     $router->get('regional-pressure', 'RegionalPressureController@getRegionalPressures');
 
@@ -114,13 +114,13 @@ $router->group(['middleware' => 'scopes:' . AuthServiceProvider::SCOPE_USER_ADMI
 // Routes for user administration
 $router->group(['middleware' => 'scopes:' . AuthServiceProvider::SCOPE_VERSION_ADMIN], function () use ($router) {
 
-        // A test route for useradmin access
-        $router->get('versionadmin', 'TeapotController@teapot');
+    // A test route for useradmin access
+    $router->get('versionadmin', 'TeapotController@teapot');
 
-        // Routes for returning information about versions
-        $router->get('version', 'VersionController@getAllVersions');
-        $router->get('version/{version:[A-Za-z0-9\.\-]+}', 'VersionController@getVersion');
+    // Routes for returning information about versions
+    $router->get('version', 'VersionController@getAllVersions');
+    $router->get('version/{version:[A-Za-z0-9\.\-]+}', 'VersionController@getVersion');
 
-        // Route for updating and creating versions
-        $router->put('version/{version:[A-Za-z0-9\.\-]+}', 'VersionController@createOrUpdateVersion');
+    // Route for updating and creating versions
+    $router->put('version/{version:[A-Za-z0-9\.\-]+}', 'VersionController@createOrUpdateVersion');
 });
