dc8b30841f8a374c74f7bf4a419b0117aa81adc4
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-01-27 21:48:51 +0000

    PSR

--- app/Models/PluginError/PluginError.php
+++ app/Models/PluginError/PluginError.php
@@ -18,6 +18,6 @@ class PluginError extends Model
     protected $fillable = [
         'user_id',
         'user_report',
-        'data'
+        'data',
     ];
 }
---
+++ tests/app/Http/Controllers/PluginErrorControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andy
+ * Date: 27/01/2019
+ * Time: 21:06
+ */
+
+namespace App\Http\Controllers;
+
+
+class PluginErrorControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
