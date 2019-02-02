9f31f716a5fe8534e49054d6a1c1e5f7a9f57b68
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-02-02 22:06:03 +0000

    Add Hold Model

---
+++ app/Models/Hold/Hold.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Models\Hold;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Model for a squawk allocation.
+ *
+ * Class Allocation
+ * @package App\Models\Squawks
+ */
+class Hold extends Model
+{
+
+    protected $table = 'hold';
+
+    public $timestamps = false;
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'fix',
+        'inbound_heading',
+        'minimum_altitude',
+        'maximum_altitude',
+        'turn_direction',
+        'description',
+    ];
+}
--- database/migrations/2019_02_02_214826_create_hold_table.php
+++ database/migrations/2019_02_02_214826_create_hold_table.php
@@ -15,16 +15,16 @@ class CreateHoldTable extends Migration
     {
         Schema::create('hold', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('identifier')
-                ->comment(
-                    'How the hold should be described, usually the holding fix, ' .
-                    'but may be different in the event of multi-purpose holds'
-                );
             $table->string('fix')->comment('The holding fix');
             $table->smallInteger('inbound_heading')->comment('The heading');
             $table->mediumInteger('minimum_altitude')->comment('The minimum altitude allowed in the hold');
             $table->mediumInteger('maximum_altitude')->comment('The maximum altitude allowed in the hold');
             $table->enum('turn_direction', ['left', 'right'])->comment('The turn direction at the holding fix');
+            $table->string('description')
+                ->comment(
+                    'How the hold should be described, usually the holding fix, ' .
+                    'but may be different in the event of multi-purpose holds'
+                );
             $table->timestamps();
         });
     }
