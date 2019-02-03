4b3733e617baca502268f211c1821d399ed4d2ee
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-02-03 18:03:40 +0000

    Add Additional Holds

--- database/migrations/2019_02_02_220619_create_standard_holds.php
+++ database/migrations/2019_02_02_220619_create_standard_holds.php
@@ -16,6 +16,7 @@ class CreateStandardHolds extends Migration
     public function up()
     {
         $holds = [
+            // EGKK
             [
                 'fix' => 'WILLO',
                 'inbound_heading' => 285,
@@ -33,7 +34,75 @@ class CreateStandardHolds extends Migration
                 'turn_direction' => 'right',
                 'description' => 'TIMBA',
                 'created_at' => Carbon::now()->toDateTimeString(),
-            ]
+            ],
+
+            // EGCC
+            [
+                'fix' => 'DAYNE',
+                'inbound_heading' => 312,
+                'minimum_altitude' => 7000,
+                'maximum_altitude' => 14000,
+                'turn_direction' => 'right',
+                'description' => 'DAYNE',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'fix' => 'ROSUN',
+                'inbound_heading' => 172,
+                'minimum_altitude' => 7000,
+                'maximum_altitude' => 24000,
+                'turn_direction' => 'right',
+                'description' => 'ROSUN',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'fix' => 'MIRSI',
+                'inbound_heading' => 62,
+                'minimum_altitude' => 6000,
+                'maximum_altitude' => 14000,
+                'turn_direction' => 'right',
+                'description' => 'MIRSI',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+
+            // EGLL
+            [
+                'fix' => 'BIG',
+                'inbound_heading' => 303,
+                'minimum_altitude' => 7000,
+                'maximum_altitude' => 15000,
+                'turn_direction' => 'right',
+                'description' => 'BIG',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'fix' => 'OCK',
+                'inbound_heading' => 329,
+                'minimum_altitude' => 7000,
+                'maximum_altitude' => 15000,
+                'turn_direction' => 'right',
+                'description' => 'OCK',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'fix' => 'BNN',
+                'inbound_heading' => 117,
+                'minimum_altitude' => 8000,
+                'maximum_altitude' => 17000,
+                'turn_direction' => 'right',
+                'description' => 'BNN',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+            [
+                'fix' => 'LAM',
+                'inbound_heading' => 263,
+                'minimum_altitude' => 8000,
+                'maximum_altitude' => 17000,
+                'turn_direction' => 'left',
+                'description' => 'LAM',
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ],
+
         ];
 
         Hold::insert($holds);
