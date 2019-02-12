2e908c797c6cd2d8afa227b808518e54b57b7d97
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-02-12 22:15:12 +0000

    Add Hold Profile Table

---
+++ database/migrations/2019_02_12_220759_create_hold_profile_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHoldProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hold_profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->json('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hold_profile');
+    }
+}
