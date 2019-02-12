11627ce264d2683c6fbb833c681c2d7aa9618e59
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-02-12 23:55:12 +0000

    Add Service Methods For Getting Hold Profiles

--- app/Services/HoldService.php
+++ app/Services/HoldService.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Models\Hold\Hold;
+use App\Models\Hold\HoldProfile;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
@@ -16,7 +18,7 @@ class HoldService
      *
      * @return array
      */
-    public function getHolds() : array
+    public function getHolds(): array
     {
         if (Cache::has(self::CACHE_KEY)) {
             return Cache::get(self::CACHE_KEY);
@@ -38,5 +40,64 @@ class HoldService
         }
 
         Log::info('Hold cache cleared');
+    }
+
+    /**
+     * Get an array of all the generic hold profiles
+     *
+     * @return array
+     */
+    public function getGenericHoldProfiles() : array
+    {
+        $profiles = HoldProfile::with('holds')
+            ->whereNull('user_id')
+            ->get()
+            ->toArray();
+
+        foreach ($profiles as $key => $profile) {
+            $profiles[$key]['holds'] = array_column($profile['holds'], 'id');
+        }
+
+        return $profiles;
+    }
+
+    /**
+     * Get an array of all the user hold profiles
+     *
+     * @return array
+     */
+    public function getUserHoldProfiles() : array
+    {
+        $profiles = HoldProfile::with('holds')
+            ->where('user_id', '=', Auth::user()->id)
+            ->get()
+            ->toArray();
+
+        foreach ($profiles as $key => $profile) {
+            $profiles[$key]['holds'] = array_column($profile['holds'], 'id');
+        }
+
+        return $profiles;
+    }
+
+    /**
+     * Get an array of the users hole profiles
+     * and the generic ones
+     *
+     * @return array
+     */
+    public function getUserAndGenericHoldProfiles() : array
+    {
+        $profiles = HoldProfile::with('holds')
+            ->where('user_id', '=', Auth::user()->id)
+            ->orWhereNull('user_id')
+            ->get()
+            ->toArray();
+
+        foreach ($profiles as $key => $profile) {
+            $profiles[$key]['holds'] = array_column($profile['holds'], 'id');
+        }
+
+        return $profiles;
     }
 }
--- tests/app/Services/HoldServiceTest.php
+++ tests/app/Services/HoldServiceTest.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\BaseFunctionalTestCase;
 use App\Models\Hold\Hold;
+use App\Models\User\User;
 use Illuminate\Support\Facades\Cache;
 
 class HoldServiceTest extends BaseFunctionalTestCase
@@ -120,10 +121,53 @@ class HoldServiceTest extends BaseFunctionalTestCase
         $this->assertEquals(['foo' => 'bar'], $this->holdService->getHolds());
     }
 
-    public function cacheClearClearsTheHoldCache()
+    public function testCacheClearClearsTheHoldCache()
     {
         Cache::shouldReceive('forget')
             ->with(HoldService::CACHE_KEY);
         $this->holdService->clearCache();
+    }
+
+    public function testGetGenericHoldProfilesReturnsGenericProfilesWithHoldIds()
+    {
+        $expected = [
+            [
+                'id' => 1,
+                'name' => 'Generic Hold Profile',
+                'holds' => [1],
+            ]
+        ];
+        $this->assertEquals($expected, $this->holdService->getGenericHoldProfiles());
+    }
+
+    public function testGetUserHoldProfilesReturnsUserProfilesWithHoldIds()
+    {
+        $this->actingAs(User::find(self::ACTIVE_USER_CID));
+        $expected = [
+            [
+                'id' => 2,
+                'name' => 'User Hold Profile',
+                'holds' => [1, 2],
+            ]
+        ];
+        $this->assertEquals($expected, $this->holdService->getUserHoldProfiles());
+    }
+
+    public function testGetUserAndGenericHoldProfilesReturnsUserAndGenericProfiles()
+    {
+        $this->actingAs(User::find(self::ACTIVE_USER_CID));
+        $expected = [
+            [
+                'id' => 1,
+                'name' => 'Generic Hold Profile',
+                'holds' => [1],
+            ],
+            [
+                'id' => 2,
+                'name' => 'User Hold Profile',
+                'holds' => [1, 2],
+            ]
+        ];
+        $this->assertEquals($expected, $this->holdService->getUserAndGenericHoldProfiles());
     }
 }
