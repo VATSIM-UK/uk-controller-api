aa108716caf1686ca6a0e71409d91054e15d4f21
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-02-24 22:19:25 +0000

    Fix #32: Add Redhill Squawks

---
+++ database/migrations/2019_02_24_221334_add_redhill_squawk.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\Squawks\Range;
+use App\Models\Squawks\SquawkRangeOwner;
+use App\Models\Squawks\SquawkUnit;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRedhillSquawk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $owner = new SquawkRangeOwner;
+        $owner->save();
+
+        SquawkUnit::create(
+            [
+                'unit' => 'EGKR',
+                'squawk_range_owner_id' => $owner->id,
+            ]
+        );
+
+        Range::create(
+            [
+                'squawk_range_owner_id' => $owner->id,
+                'start' => '3767',
+                'stop' => '3767',
+                'rules' => 'A',
+                'allow_duplicate' => true,
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Nothing to do
+    }
+}
