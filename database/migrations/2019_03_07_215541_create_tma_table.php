9ad6e1ae6e1b41a70c16dabbe73fe360543a0cdc
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-03-07 22:12:58 +0000

    Add TMA Table

---
+++ database/migrations/2019_03_07_215541_create_tma_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTmaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tma', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->comment('The TMA name');
+            $table->string('description')->comment('Description of the TMA');
+            $table->unsignedMediumInteger('transition_altitude');
+            $table->timestamps();
+
+            $table->unique('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tma');
+    }
+}
