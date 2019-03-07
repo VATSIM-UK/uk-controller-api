fb520e3f0a5f403f948f8c62733849de487ae136
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-03-07 22:12:26 +0000

    Add Airfield Table

---
+++ database/migrations/2019_03_07_214624_create_airfield_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAirfieldTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('airfield', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code')->comment('ICAO code for the airfield');
+            $table->unsignedMediumInteger('transition_altitude')->comment('The transition altitude');
+            $table->timestamps();
+
+            $table->unique('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('airfield');
+    }
+}
