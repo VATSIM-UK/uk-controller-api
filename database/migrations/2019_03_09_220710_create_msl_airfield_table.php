2d346a3f7107dfcd6933e3cdb8ef97f4b63db78e
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-03-09 22:09:29 +0000

    Add Migrations For MSL Values Tables

---
+++ database/migrations/2019_03_09_220710_create_msl_airfield_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMslAirfieldTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('msl_airfield', function (Blueprint $table) {
+            $table->unsignedInteger('airfield_id');
+            $table->unsignedSmallInteger('msl');
+            $table->timestamp('generated_at');
+
+            $table->unique('tma_id');
+            $table->foreign('airfield_id')->references('id')->on('airfield');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('msl_airfield');
+    }
+}
