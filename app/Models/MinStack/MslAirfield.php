86017d5b740f5b4a432a7bdd995f0d70eb2c7512
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-03-09 22:28:31 +0000

    Disable Timestamps On Model

--- app/Models/MinStack/MslAirfield.php
+++ app/Models/MinStack/MslAirfield.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MslAirfield extends Model
 {
+    public $timestamps = false;
+
     protected $table = 'msl_airfield';
 
     protected $fillable = [
--- app/Models/MinStack/MslTma.php
+++ app/Models/MinStack/MslTma.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MslTma extends Model
 {
+    public $timestamps = false;
+
     protected $table = 'msl_tma';
 
     protected $fillable = [
