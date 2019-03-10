2af022354028d8fee48e09602d28199b19a93a2a
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-03-10 01:14:16 +0000

    MinStackCalculator Can Calculate Directs And Lowests

--- app/Helpers/MinStackLevel/MinStackCalculator.php
+++ app/Helpers/MinStackLevel/MinStackCalculator.php
@@ -2,10 +2,34 @@
 
 namespace App\Helpers\MinStackLevel;
 
+use App\Models\Airfield;
+use App\Services\MetarService;
+
 class MinStackCalculator
 {
+    // Pressure boundaries in MSL calculations
     const STANDARD_PRESSURE = 1013;
     const LOW_PRESSURE_BOUNDARY = 978;
+
+    // The calculation type where it's a direct airfield calculation
+    const CALCULATION_TYPE_AIRFIELD = 'airfield';
+
+    // The calculation type where we want the lowest QNH of many
+    const CALCULATION_TYPE_LOWEST = 'lowest-of';
+
+    /**
+     * @var MetarService
+     */
+    private $metarService;
+
+    /**
+     * MinStackCalculator constructor.
+     * @param MetarService $metarService
+     */
+    public function __construct(MetarService $metarService)
+    {
+        $this->metarService = $metarService;
+    }
 
     /**
      * @param int $transitionAltitude
@@ -24,5 +48,69 @@ class MinStackCalculator
         }
 
         return $transitionAltitude + 3000;
+    }
+
+    /**
+     * Calculate the min stack level in a direct relation.
+     *
+     * @param string $code
+     * @param int $transitionAltitude
+     * @param bool $standardHigh
+     * @return int|null
+     */
+    public function calculateDirectMinStack(string $code, int $transitionAltitude, bool $standardHigh) : ?int
+    {
+        $qnh = $this->metarService->getQnhFromVatsimMetar($code);
+
+        if ($qnh === null) {
+            return null;
+        }
+
+        return self::calculateMinStack(
+            $transitionAltitude,
+            $qnh,
+            $standardHigh
+        );
+    }
+
+    /**
+     * Calculate the MinStack where an airfields MinStack is defined based on the lowest
+     * QNH in a group.
+     *
+     * @param int $transitionAltitude
+     * @param bool $standardHigh
+     * @param array $airfields
+     * @return int|null
+     */
+    public function calculateLowestQnhMinStack(array $airfields): ?int
+    {
+        $qnhs = [];
+        foreach ($airfields as $icao) {
+            $qnh = $this->metarService->getQnhFromVatsimMetar($icao);
+            if ($qnh === null) {
+                continue;
+            }
+
+            $qnhs[$icao] = $qnh;
+        }
+
+        // No QNHs, stop
+        if (empty($qnhs)) {
+            return null;
+        }
+
+        // Get the airfield with the lowest QNH and calculate MSL
+        $calculationIcao = array_keys($qnhs, min($qnhs))[0];
+        $airfield = Airfield::where('code', $calculationIcao)->first();
+
+        if ($airfield === null) {
+            return null;
+        }
+
+        return self::calculateMinStack(
+            $airfield->transition_altitude,
+            min($qnhs),
+            $airfield->standard_high
+        );
     }
 }
--- tests/app/Helpers/MinStack/MinStackCalculatorTest.php
+++ tests/app/Helpers/MinStack/MinStackCalculatorTest.php
@@ -1,10 +1,29 @@
 <?php
 
-use App\BaseUnitTestCase;
+use App\BaseFunctionalTestCase;
 use App\Helpers\MinStackLevel\MinStackCalculator;
+use App\Services\MetarService;
+use Mockery\MockInterface;
 
-class MinStackCalculatorTest extends BaseUnitTestCase
+class MinStackCalculatorTest extends BaseFunctionalTestCase
 {
+    /**
+     * @var MockInterface
+     */
+    private $metarService;
+
+    /**
+     * @var MinStackCalculator
+     */
+    private $calculator;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->metarService = Mockery::mock(MetarService::class);
+        $this->calculator = new MinStackCalculator($this->metarService);
+    }
+
     public function testItReturnsTransitionPlus1000OnHighPressure()
     {
         $this->assertEquals(7000, MinStackCalculator::calculateMinStack(6000, 1014, true));
@@ -28,5 +47,45 @@ class MinStackCalculatorTest extends BaseUnitTestCase
     public function testItReturnsTransitionPlus3000OnDoubleLow()
     {
         $this->assertEquals(9000, MinStackCalculator::calculateMinStack(6000, 977, false));
+    }
+
+    public function testItCalculatesDirectMinStack()
+    {
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGLL')->andReturn(1015);
+        $this->assertEquals(7000, $this->calculator->calculateDirectMinStack('EGLL', 6000, true));
+    }
+
+    public function testItReturnsNullIfNoDirectMinStackQNH()
+    {
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGLL')->andReturn(null);
+        $this->assertNull($this->calculator->calculateDirectMinStack('EGLL', 6000, true));
+    }
+
+    public function testItCalculatesLowestMinStack()
+    {
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGLL')->andReturn(1015);
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGBB')->andReturn(1012);
+        $this->assertEquals(8000, $this->calculator->calculateLowestQnhMinStack(['EGLL', 'EGBB']));
+    }
+
+    public function testItReturnsNullNoQnhsAvailable()
+    {
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGLL')->andReturn(null);
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGBB')->andReturn(null);
+        $this->assertNull($this->calculator->calculateLowestQnhMinStack(['EGLL', 'EGBB']));
+    }
+
+    public function testItHandlesMissingQnhsWhenDoingLowest()
+    {
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGLL')->andReturn(1013);
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGBB')->andReturn(null);
+        $this->assertEquals(7000, $this->calculator->calculateLowestQnhMinStack(['EGLL', 'EGBB']));
+    }
+
+    public function testItReturnsNullForNonExistantAirfieldsWhenDoingLowest()
+    {
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGLL')->andReturn(1013);
+        $this->metarService->shouldReceive('getQnhFromVatsimMetar')->with('EGGG')->andReturn(1012);
+        $this->assertNull($this->calculator->calculateLowestQnhMinStack(['EGLL', 'EGGG']));
     }
 }
