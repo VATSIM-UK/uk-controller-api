64d1ecc516cc504d000f045ce8cf0f12f8f92c90
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-05-22 21:58:25 +0100

    Update Packages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 try {
-    (new Dotenv\Dotenv(__DIR__.'/../'))->load();
+    Dotenv\DotEnv::create(__DIR__.'/../')->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
