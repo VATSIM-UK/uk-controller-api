e45681ce78ae051bf81c781c3092114390925b70
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-05-22 22:00:05 +0100

    Fix BroadcastController
    The wrong Request type made it not work

--- app/Http/Controllers/BroadcastController.php
+++ app/Http/Controllers/BroadcastController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Broadcast;
-use Laravel\Lumen\Http\Request;
 
 class BroadcastController extends BaseController
 {
