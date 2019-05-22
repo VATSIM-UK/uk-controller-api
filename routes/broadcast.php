e16ba6af3996302a03e25e29f14c12186414de53
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-05-22 22:00:22 +0100

    Fix Broadcast Routes

--- routes/broadcast.php
+++ routes/broadcast.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\User\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('test', function () {
-    return true;
+Broadcast::channel('minstack-updates', function (User $user) {
+    return $user->exists();
+});
+
+Broadcast::channel('notifications-{position}', function (User $user) {
+    return $user->exists();
 });
