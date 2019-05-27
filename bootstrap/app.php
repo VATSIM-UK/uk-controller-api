2a365d4d2895442fb0a29167737603d975ede9a5
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-05-27 15:26:30 +0100

    Bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,19 +19,9 @@ try {
 |
 */
 
-$app = new Laravel\Lumen\Application(
+$app = new \Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
-
-$app->withFacades();
-
-$app->withEloquent();
-
-
-// Laravel config
-
-$app->configure('auth');
-$app->configure('filesystems');
 
 /*
 |--------------------------------------------------------------------------
@@ -54,6 +44,11 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
 $app->bind(
     \App\Services\ManifestService::class,
     function ($app) {
@@ -61,91 +56,14 @@ $app->bind(
     }
 );
 
+$app->bind(\App\Services\SquawkService::class, function ($app) {
+    return new \App\Services\SquawkService();
+});
+
 $app->bind(
     \App\Services\VersionService::class,
     function ($app) {
         return new \App\Services\VersionService($app);
-    }
-);
-
-/*
-|--------------------------------------------------------------------------
-| Register Middleware
-|--------------------------------------------------------------------------
-|
-| Next, we will register the middleware with the application. These can
-| be global middleware that run before and after each request into a
-| route or middleware that'll be assigned to some specific routes.
-|
-*/
-
-// $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
-// ]);
-
-$app->middleware(
-    [
-        'auth' => App\Http\Middleware\Authenticate::class,
-        'user.banned' => App\Http\Middleware\UserIsBanned::class,
-        'user.disabled' => App\Http\Middleware\UserIsDisabled::class,
-    ]
-);
-
-$app->routeMiddleware(
-    [
-        'user.lastlogin' => App\Http\Middleware\UserLastLogin::class,
-        'user.version' => App\Http\Middleware\UserPluginVersion::class,
-        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
-        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
-        'vatsim.cid' => App\Http\Middleware\VatsimCid::class,
-    ]
-);
-
-/*
-|--------------------------------------------------------------------------
-| Register Service Providers
-|--------------------------------------------------------------------------
-|
-| Here we will register all of the application's service providers which
-| are used to bind services into the container. Service providers are
-| totally optional, so you are not required to uncomment this line.
-|
-*/
-
-// $app->register(App\Providers\AppServiceProvider::class);
-$app->register(Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class);
-$app->register(App\Providers\AuthServiceProvider::class);
-$app->register(App\Providers\BroadcastServiceProvider::class);
-$app->register(App\Providers\EventServiceProvider::class);
-$app->bind(\App\Providers\SquawkService::class, function ($app) {
-    return new \App\Providers\SquawkService();
-});
-$app->register(Laravel\Passport\PassportServiceProvider::class);
-$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
-$app->register(Illuminate\Filesystem\FilesystemServiceProvider::class);
-$app->register(\App\Providers\RegionalPressureServiceProvider::class);
-$app->register(\App\Providers\MinStackCalculationServiceProvider::class);
-$app->register(\Illuminate\Redis\RedisServiceProvider::class);
-if (class_exists('Vluzrmos\Tinker\TinkerServiceProvider')) {
-    $app->register(Vluzrmos\Tinker\TinkerServiceProvider::class);
-}
-/*
-|--------------------------------------------------------------------------
-| Load The Application Routes
-|--------------------------------------------------------------------------
-|
-| Next we will include the routes file so that they can all be added to
-| the application. This will provide all of the URLs the application
-| can respond to, as well as the controllers that may handle them.
-|
-*/
-
-$app->router->group(
-    [
-    'namespace' => 'App\Http\Controllers',
-    ],
-    function ($router) {
-        include __DIR__.'/../routes/routes.php';
     }
 );
 
