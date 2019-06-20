31349997442f0b64a7db516891de0a25df418154
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-06-21 00:46:36 +0100

    Add Sid-Airfield Relation

--- app/Models/Sid.php
+++ app/Models/Sid.php
@@ -3,14 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Sid extends Model
 {
-    protected $timestamps = true;
+    public $timestamps = true;
+
+    public $table = 'sid';
 
     protected $fillable = [
         'airfield_id',
         'identifier',
         'initial_altitude'
     ];
+
+    /**
+     * @return HasOne
+     */
+    public function airfield() : HasOne
+    {
+        return $this->hasOne(Airfield::class);
+    }
 }
