203e19655edddafcf6842e970f3a9c2079c06bfa
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-06-21 00:46:12 +0100

    Add Missing Airfields

---
+++ database/migrations/2019_06_20_231817_add_missing_airfields.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Airfield;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Carbon;
+
+class AddMissingAirfields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->getAirfieldData() as $airfieldDatum) {
+            Airfield::insert($airfieldDatum);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        foreach ($this->getAirfieldData() as $airfieldDatum) {
+            Airfield::where('code', $airfieldDatum['code'])->firstOrFail()->delete();
+        }
+    }
+
+    private function getAirfieldData() : array
+    {
+        return [
+            [
+                'code' => 'EGWU',
+                'transition_altitude' => 6000,
+                'standard_high' => true,
+                'msl_calculation' => '{"type": "direct", "airfield": "EGLL"}',
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'code' => 'EGPD',
+                'transition_altitude' => 6000,
+                'standard_high' => false,
+                'msl_calculation' => '{"type": "direct", "airfield": "EGPD"}',
+                'created_at' => Carbon::now(),
+            ],
+        ];
+    }
+}
