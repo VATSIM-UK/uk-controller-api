3a35bdb477424dd6a5819e5b8ab3f235f6ba7426
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-06-21 04:01:37 +0100

    Create Table Of Admins

---
+++ database/migrations/2019_06_21_025916_create_admin_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->primary();
+            $table->string('email')->comment('Email address for user, used as username');
+            $table->string('password')->comment('Hashed admin password for the user');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+}
