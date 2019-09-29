aaac6421a5fa2032810efc8a00935159cef96490
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-09-29 17:55:50 +0100

    Migration For Creating The Handoff Order Table

---
+++ database/migrations/2019_09_29_134129_create_handoff_orders_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHandoffOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('handoff_orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('handoff_id')
+                ->comment('The handoff order that the entry belongs to');
+
+            $table->unsignedBigInteger('controller_position_id')
+                ->comment('The controller position');
+
+            $table->unsignedTinyInteger('order')
+                ->comment('The place of the controller position in the handoff order');
+
+            $table->timestamps();
+
+            // Unique keys
+            $table->unique(['handoff_id', 'controller_position_id']);
+            $table->unique(['handoff_id', 'order']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('handoff_orders');
+    }
+}
