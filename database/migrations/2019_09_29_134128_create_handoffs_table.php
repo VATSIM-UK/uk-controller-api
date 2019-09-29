9669e9e3299b4a64a5d85c3895762919fbf74a5a
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-09-29 17:54:37 +0100

    Create Handoffs Table

--- database/migrations/2019_09_29_134128_create_handoffs_table.php
+++ database/migrations/2019_09_29_134128_create_handoffs_table.php
@@ -15,25 +15,12 @@ class CreateHandoffsTable extends Migration
     {
         Schema::create('handoffs', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedInteger('sid_id');
-            $table->unsignedBigInteger('controller_position_id');
-            $table->unsignedTinyInteger('order')->comment('The place on the handoff order of this controller');
+            $table->string('key');
+            $table->string('description');
             $table->timestamps();
 
-            // Foreign references
-            $table->foreign('sid_id')
-                ->references('id')
-                ->on('sid')
-                ->onDelete('cascade');
-
-            $table->foreign('controller_position_id')
-                ->references('id')
-                ->on('controller_positions')
-                ->onDelete('cascade');
-
             // Unique keys
-            $table->unique(['sid_id', 'order']);
-            $table->unique(['sid_id', 'controller_position_id']);
+            $table->unique('key');
         });
     }
 
