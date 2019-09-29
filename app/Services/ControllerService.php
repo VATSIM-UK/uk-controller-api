3eb92d44cdea457431284ce2a46ebd1dda62dd2d
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-09-29 02:57:47 +0100

    Add Service For Generating Legacy Dependencies

--- app/Services/ControllerService.php
+++ app/Services/ControllerService.php
@@ -1,10 +1,52 @@
 <?php
 
-
 namespace App\Services;
 
+use App\Models\Airfield;
+use App\Models\Controller\ControllerPosition;
 
 class ControllerService
 {
+    /**
+     * Get the legacy dependency as represented in controller-positions.json.
+     *
+     * @return array
+     */
+    public function getLegacyControllerPositionsDependency() : array
+    {
+        $controllers = ControllerPosition::with('topDownAirfields')->get();
+        $dependency = [];
+        $controllers->each(function (ControllerPosition $controller) use (&$dependency) {
+            $dependency[$controller->callsign] = [
+                'frequency' => (float) $controller->frequency,
+            ];
 
+            $controller->topDownAirfields->each(function(Airfield $airfield) use ($controller, &$dependency) {
+                $dependency[$controller->callsign]['top-down'][] = $airfield->code;
+            });
+        });
+
+        return $dependency;
+    }
+
+    /**
+     * Get the legacy dependency as represented in airfield-ownership.json.
+     *
+     * @return array
+     */
+    public function getLegacyAirfieldOwnershipDependency() : array
+    {
+        $airfields = Airfield::with('controllers')->get();
+
+        $dependency = [];
+        $airfields->each(function (Airfield $airfield) use (&$dependency) {
+            $controllers = $airfield->controllers()->orderBy('order')->get();
+
+            $controllers->each(function (ControllerPosition $controller) use ($airfield, &$dependency) {
+                $dependency[$airfield->code][] = $controller->callsign;
+            });
+        });
+
+        return $dependency;
+    }
 }
