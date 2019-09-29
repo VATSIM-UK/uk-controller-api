80b9a41820929bcb901cc7507a4235555fa4aa0e
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-09-29 14:46:50 +0100

    Add Table For SID Handoffs

---
+++ database/migrations/2019_09_29_134128_create_handoffs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHandoffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('handoffs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('sid_id');
+            $table->unsignedBigInteger('controller_position_id');
+            $table->unsignedTinyInteger('order')->comment('The place on the handoff order of this controller');
+            $table->timestamps();
+
+            // Foreign references
+            $table->foreign('sid_id')->references('id')->on('sid')->onDelete('cascade');
+            $table->foreign('controller_id')->references('id')->on('controller_positions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('handoffs');
+    }
+}
