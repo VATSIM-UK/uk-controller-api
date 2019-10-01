bb136b249639d85f96eed7f016fec2f0d223221d
Andy Ford <TheHungryPredator@hotmail.co.uk>
2019-10-01 21:50:05 +0100

    Add Dependency Table

---
+++ database/migrations/2019_10_01_204600_add_dependencies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDependenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dependencies', function(Blueprint $table) {
+            $table->mediumIncrements('id');
+            $table->string('location')->comment('Where the dependency can be downloaded from');
+            $table->string('local_file')->comment('The local file where the dependency should be stored');
+            $table->timestamps();
+
+            // Keys
+            $table->unique('local_file');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dependencies');
+    }
+}
