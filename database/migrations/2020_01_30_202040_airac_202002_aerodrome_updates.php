66c174e748126cbe24ac87c0fc4b48c82974090d
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-02-01 15:53:31 +0000

    Move Aerodrome Updates To Use New Services

--- database/migrations/2020_01_30_202040_airac_202002_aerodrome_updates.php
+++ database/migrations/2020_01_30_202040_airac_202002_aerodrome_updates.php
@@ -2,6 +2,7 @@
 
 use App\Models\Airfield\Airfield;
 use App\Models\Controller\ControllerPosition;
+use App\Services\AirfieldService;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
@@ -14,70 +15,27 @@ class Airac202002AerodromeUpdates extends Migration
      */
     public function up()
     {
-        // Frequency updates
-        ControllerPosition::where('callsign', 'EGBB_TWR')->update(['frequency' => 123.97]);
-        ControllerPosition::where('callsign', 'EGMC_TWR')->update(['frequency' => 127.75]);
+        try {
+            DB::beginTransaction();
+            // Frequency updates
+            ControllerPosition::where('callsign', 'EGBB_TWR')->update(['frequency' => 123.97]);
+            ControllerPosition::where('callsign', 'EGMC_TWR')->update(['frequency' => 127.75]);
 
-        // Create LF GND position
-        $lfGnd = ControllerPosition::create(
-            [
-                'callsign' => 'EGLF_GND',
-                'frequency' => 121.82,
-            ]
-        );
+            // Create LF GND position
+            ControllerPosition::create(
+                [
+                    'callsign' => 'EGLF_GND',
+                    'frequency' => 121.82,
+                ]
+            );
 
-        // Update LF top-down
-        $lf = Airfield::where('code', 'EGLF')->firstOrFail();
-        DB::table('top_downs')->where('airfield_id', $lf->id)->delete();
-        DB::table('top_downs')->insert(
-            [
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'EGLF_GND')->firstOrFail()->id,
-                    'order' => 1,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'EGLF_TWR')->firstOrFail()->id,
-                    'order' => 2,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'EGLF_APP')->firstOrFail()->id,
-                    'order' => 3,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LTC_SW_CTR')->firstOrFail()->id,
-                    'order' => 4,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LTC_S_CTR')->firstOrFail()->id,
-                    'order' => 5,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LTC_CTR')->firstOrFail()->id,
-                    'order' => 6,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LON_S_CTR')->firstOrFail()->id,
-                    'order' => 7,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LON_SC_CTR')->firstOrFail()->id,
-                    'order' => 8,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LON_CTR')->firstOrFail()->id,
-                    'order' => 9,
-                ],
-            ]
-        );
+            // Update LF top-down
+            AirfieldService::insertIntoOrderBefore('EGLF', 'EGLF_GND', 'EGLF_TWR');
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
     }
 
     /**
@@ -87,58 +45,20 @@ class Airac202002AerodromeUpdates extends Migration
      */
     public function down()
     {
-        ControllerPosition::where('callsign', 'EGBB_TWR')->update(['frequency' => 118.3]);
-        ControllerPosition::where('callsign', 'EGMC_TWR')->update(['frequency' => 126.72]);
+        try {
+            DB::beginTransaction();
+            ControllerPosition::where('callsign', 'EGBB_TWR')->update(['frequency' => 118.3]);
+            ControllerPosition::where('callsign', 'EGMC_TWR')->update(['frequency' => 126.72]);
 
-        // Delete LF GND
-        ControllerPosition::where('callsign', 'EGLF_GND')->firstOrFail()->delete();
+            // Update LF top-down
+            AirfieldService::removeFromTopDownsOrder('EGLF', 'EGLF_GND');
 
-        // Update LF top-down
-        $lf = Airfield::where('code', 'EGLF')->firstOrFail();
-        DB::table('top_downs')->where('airfield_id', $lf->id)->delete();
-        DB::table('top_downs')->insert(
-            [
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'EGLF_TWR')->firstOrFail()->id,
-                    'order' => 1,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'EGLF_APP')->firstOrFail()->id,
-                    'order' => 2,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LTC_SW_CTR')->firstOrFail()->id,
-                    'order' => 3,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LTC_S_CTR')->firstOrFail()->id,
-                    'order' => 4,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LTC_CTR')->firstOrFail()->id,
-                    'order' => 5,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LON_S_CTR')->firstOrFail()->id,
-                    'order' => 6,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LON_SC_CTR')->firstOrFail()->id,
-                    'order' => 7,
-                ],
-                [
-                    'airfield_id' => $lf->id,
-                    'controller_position_id' => ControllerPosition::where('callsign', 'LON_CTR')->firstOrFail()->id,
-                    'order' => 8,
-                ],
-            ]
-        );
+            // Delete LF GND
+            ControllerPosition::where('callsign', 'EGLF_GND')->firstOrFail()->delete();
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
     }
 }
