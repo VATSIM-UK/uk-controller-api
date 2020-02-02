eb5269959b5cee8ecbc61f75d6ca471e1f53bb74
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-02-02 02:46:15 +0000

    Config Stuff

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -67,6 +67,7 @@ class GithubController
     {
         $labels = $issue['labels'] ?? [];
         $numCreated = 0;
+        log(config('github'));
         foreach ($labels as $label) {
             if ($label['name'] == config('github.plugin.label') && !$databaseIssue->plugin) {
                 $createdPlugin = $this->createGithubIssue($label['name'], $issue['title'], $issue['html_url']);
