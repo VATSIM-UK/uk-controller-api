729af2b3bedab0d69f1c7429e745e8d7d63234c4
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-02-05 19:40:51 +0000

    Add Seeder To Keep Tables Tidy

---
+++ database/seeds/SectorFileIssuesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\User\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SectorFileIssuesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
