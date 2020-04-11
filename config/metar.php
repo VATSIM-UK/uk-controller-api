321412df33a2cf9a97faa68591ce4335bc6d048b
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-11 17:12:45 +0100

    Provide Default

--- config/metar.php
+++ config/metar.php
@@ -3,5 +3,5 @@
 // Config for downloading METARs in the context of MSL and RPS
 return [
     'vatsim_url' => env('VATSIM_METAR_URL', 'metar.vatsim.net'),
-    'regional_url' => env('APP_REGIONAL_PRESSURES_URL'),
+    'regional_url' => env('APP_REGIONAL_PRESSURES_URL', 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requesttype=retrieve&format=xml&hoursBeforeNow=3&mostRecentForEachStation=constraint&stationString=~gb&fields=raw_text,station_id'),
 ];
