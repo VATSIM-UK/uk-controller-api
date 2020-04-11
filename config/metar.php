cb96c0935f5a330238ac1d83098cf8535bf71765
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-11 16:07:12 +0100

    Comment To Trigger CI

--- config/metar.php
+++ config/metar.php
@@ -1,5 +1,6 @@
 <?php
 
+// Config for downloading METARs in the context of MSL and RPS
 return [
     'vatsim_url' => env('VATSIM_METAR_URL', 'metar.vatsim.net'),
     'regional_url' => env('APP_REGIONAL_PRESSURES_URL'),
