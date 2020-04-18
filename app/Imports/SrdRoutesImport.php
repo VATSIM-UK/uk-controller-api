4c1b69a5329fbf6ec1b6a30f3d8a3f4f74653153
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-18 18:39:56 +0100

    Import SrdRoutes As Collection

--- app/Imports/SrdRoutesImport.php
+++ app/Imports/SrdRoutesImport.php
@@ -3,36 +3,35 @@
 namespace App\Imports;
 
 use App\Models\Srd\SrdRoute;
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithProgressBar;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 use Maatwebsite\Excel\Events\BeforeSheet;
 
-class SrdRoutesImport implements ToModel, WithStartRow, WithProgressBar, WithEvents
+class SrdRoutesImport implements ToCollection, WithStartRow, WithProgressBar, WithEvents
 {
     use Importable;
 
     const FL_CONVERSION_FACTOR = 100;
     const FL_MIN_CRUISE = 'MC';
 
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        return new SrdRoute([
-            'origin' => $row[0],
-            'destination' => $row[6],
-            'min_level' => $this->convertFlightLevel($row[2]),
-            'max_level' => $this->convertFlightLevel($row[3]),
-            'route_segment' => $row[4] ?? '',
-            'sid' => $row[1],
-            'star' => $row[5],
-        ]);
+        foreach ($rows as $row) {
+            $route = SrdRoute::create([
+                'origin' => $row[0],
+                'destination' => $row[6],
+                'minimum_level' => $this->convertFlightLevel($row[2]),
+                'maximum_level' => $this->convertFlightLevel($row[3]),
+                'route_segment' => $row[4] ?? '',
+                'sid' => $row[1],
+                'star' => $row[5],
+            ]);
+        }
     }
 
     private function convertFlightLevel(string $flightLevel): ?int
--- tests/app/Imports/SrdRoutesImportTest.php
+++ tests/app/Imports/SrdRoutesImportTest.php
@@ -1,76 +1,151 @@
 <?php
 
-namespace app\Imports;
+namespace App\Imports;
 
-use App\BaseUnitTestCase;
+use App\BaseFunctionalTestCase;
+use App\Models\Srd\SrdRoute;
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Events\BeforeSheet;
 
-class SrdRoutesImportTest extends BaseUnitTestCase
+class SrdRoutesImportTest extends BaseFunctionalTestCase
 {
-    public function testItReturnsCorrectModel()
+    public function setUp(): void
     {
-        $data = [
-            'EGGD',
-            'WOTAN1Z',
-            '140',
-            '150',
-            'L9 KENET',
-            'OCK1A',
-            'EGLL'
-        ];
+        parent::setUp();
+        // Empty the table first
+        SrdRoute::all()->each(function (SrdRoute $route) {
+            $route->delete();
+        });
+    }
 
-        $model = (new SrdRoutesImport())->model($data);
+    public function testItProcessesModel()
+    {
+        $collection = new Collection();
+        $collection->push(
+            [
+                'EGGD',
+                'WOTAN',
+                '140',
+                '150',
+                'L9 KENET',
+                'OCK1A',
+                'EGLL',
+            ]
+        );
+
+        (new SrdRoutesImport())->collection($collection);
+        $this->assertCount(1, SrdRoute::all());
+
+        $model = SrdRoute::where('origin', 'EGGD')->first();
         $this->assertEquals('EGGD', $model->origin);
-        $this->assertEquals('EGLL', $model->destomatopm);
+        $this->assertEquals('EGLL', $model->destination);
         $this->assertEquals('WOTAN', $model->sid);
         $this->assertEquals('OCK1A', $model->star);
         $this->assertSame(14000, $model->minimum_level);
-        $this->assertSame(15000, $model->minimum_level);
+        $this->assertSame(15000, $model->maximum_level);
         $this->assertEquals('L9 KENET', $model->route_segment);
     }
 
-    public function testItReturnsCorrectWithMcMinimum()
+    public function testItProcessesMinimumCruise()
     {
-        $data = [
-            'EGGD',
-            'WOTAN1Z',
-            'MC',
-            '150',
-            'L9 KENET',
-            'OCK1A',
-            'EGLL'
-        ];
+        $collection = new Collection();
+        $collection->push(
+            [
+                'EGGD',
+                'WOTAN',
+                'MC',
+                '150',
+                'L9 KENET',
+                'OCK1A',
+                'EGLL',
+            ]
+        );
 
-        $model = (new SrdRoutesImport())->model($data);
+        (new SrdRoutesImport())->collection($collection);
+        $this->assertCount(1, SrdRoute::all());
+
+        $model = SrdRoute::where('origin', 'EGGD')->first();
         $this->assertEquals('EGGD', $model->origin);
-        $this->assertEquals('EGLL', $model->destomatopm);
+        $this->assertEquals('EGLL', $model->destination);
         $this->assertEquals('WOTAN', $model->sid);
         $this->assertEquals('OCK1A', $model->star);
         $this->assertNull($model->minimum_level);
-        $this->assertSame(15000, $model->minimum_level);
+        $this->assertSame(15000, $model->maximum_level);
         $this->assertEquals('L9 KENET', $model->route_segment);
     }
 
-    public function testItReturnsCorrectWithNoRouteString()
+    public function testItProcessesNoRouteString()
     {
-        $data = [
+        $collection = new Collection();
+        $collection->push([
             'EGGD',
-            'WOTAN1Z',
+            'WOTAN',
             'MC',
             '150',
             null,
             'OCK1A',
-            'EGLL'
-        ];
+            'EGLL',
+        ]);
 
-        $model = (new SrdRoutesImport())->model($data);
+        (new SrdRoutesImport())->collection($collection);
+        $this->assertCount(1, SrdRoute::all());
+
+        $model = SrdRoute::where('origin', 'EGGD')->first();
         $this->assertEquals('EGGD', $model->origin);
-        $this->assertEquals('EGLL', $model->destomatopm);
+        $this->assertEquals('EGLL', $model->destination);
         $this->assertEquals('WOTAN', $model->sid);
         $this->assertEquals('OCK1A', $model->star);
-        $this->assertSame(0, $model->minimum_level);
-        $this->assertSame(15000, $model->minimum_level);
+        $this->assertNull($model->minimum_level);
+        $this->assertSame(15000, $model->maximum_level);
         $this->assertEquals('', $model->route_segment);
+    }
+
+    public function testItProcessesMultipleModels()
+    {
+        $collection = new Collection();
+        $collection->push(
+            [
+                'EGGD',
+                'WOTAN',
+                '140',
+                '150',
+                'L9 KENET',
+                'OCK1A',
+                'EGLL',
+            ]
+        );
+        $collection->push(
+            [
+                'EGFF',
+                'ALVIN',
+                '140',
+                '150',
+                'L9 KENET',
+                'OCK1A',
+                'EGLL',
+            ]
+        );
+
+        (new SrdRoutesImport())->collection($collection);
+        $this->assertCount(2, SrdRoute::all());
+
+        $model1 = SrdRoute::where('origin', 'EGGD')->first();
+        $this->assertEquals('EGGD', $model1->origin);
+        $this->assertEquals('EGLL', $model1->destination);
+        $this->assertEquals('WOTAN', $model1->sid);
+        $this->assertEquals('OCK1A', $model1->star);
+        $this->assertSame(14000, $model1->minimum_level);
+        $this->assertSame(15000, $model1->maximum_level);
+        $this->assertEquals('L9 KENET', $model1->route_segment);
+
+        $model2 = SrdRoute::where('origin', 'EGFF')->first();
+        $this->assertEquals('EGFF', $model2->origin);
+        $this->assertEquals('EGLL', $model2->destination);
+        $this->assertEquals('ALVIN', $model2->sid);
+        $this->assertEquals('OCK1A', $model2->star);
+        $this->assertSame(14000, $model2->minimum_level);
+        $this->assertSame(15000, $model2->maximum_level);
+        $this->assertEquals('L9 KENET', $model2->route_segment);
     }
 
     public function testItStartsOnRowTwo()
