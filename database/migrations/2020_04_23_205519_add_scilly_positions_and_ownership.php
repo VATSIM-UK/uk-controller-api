1d2b2d92de8aa92319c9983bd9ddff759ebf9b9f
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-23 22:13:22 +0100

    Remove Unused Imports

--- database/migrations/2020_04_23_205519_add_scilly_positions_and_ownership.php
+++ database/migrations/2020_04_23_205519_add_scilly_positions_and_ownership.php
@@ -4,9 +4,7 @@ use App\Services\AirfieldService;
 use App\Services\DependencyService;
 use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class AddScillyPositionsAndOwnership extends Migration
 {
--- database/migrations/2020_04_23_210257_add_brize_lars_position.php
+++ database/migrations/2020_04_23_210257_add_brize_lars_position.php
@@ -3,9 +3,7 @@
 use App\Services\DependencyService;
 use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class AddBrizeLarsPosition extends Migration
 {
