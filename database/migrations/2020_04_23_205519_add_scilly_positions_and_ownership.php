fc8986937c3037ad45722b73178444a0bacdc467
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-23 22:01:24 +0100

    Fix #133: Add Scilly Positions and Top Down

---
+++ database/migrations/2020_04_23_205519_add_scilly_positions_and_ownership.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Services\AirfieldService;
+use App\Services\DependencyService;
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddScillyPositionsAndOwnership extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('controller_positions')
+            ->insert(
+                [
+                    [
+                        'callsign' => 'EGHE_TWR',
+                        'frequency' => 124.87,
+                        'created_at' => Carbon::now(),
+                    ],
+                    [
+                        'callsign' => 'EGHE_APP',
+                        'frequency' => 124.87,
+                        'created_at' => Carbon::now(),
+                    ],
+                ]
+            );
+
+        AirfieldService::createNewTopDownOrder('EGHE', ['EGHE_TWR', 'EGHE_APP', 'LON_W_CTR', 'LON_CTR']);
+        DependencyService::touchDependencyByKey('DEPENDENCY_CONTROLLER_POSITIONS');
+        DependencyService::touchDependencyByKey('DEPENDENCY_AIRFIELD_OWNERSHIP');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        AirfieldService::deleteTopDownOrder('EGHE');
+
+        DB::table('controller_positions')
+            ->whereIn('callsign', ['EGHE_TWR', 'EGHE_APP'])
+            ->delete();
+
+        DependencyService::touchDependencyByKey('DEPENDENCY_CONTROLLER_POSITIONS');
+        DependencyService::touchDependencyByKey('DEPENDENCY_AIRFIELD_OWNERSHIP');
+    }
+}
