27abc4a594da126d619e1b63cc288c1392bde473
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-04-23 21:12:07 +0100

    Fix #136: London Mil and Fighter Control

---
+++ database/migrations/2020_04_23_195443_update_military_positions.php
@@ -0,0 +1,80 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateMilitaryPositions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Delete the old positions
+        DB::table('controller_positions')
+            ->whereIn('callsign', ['EGVV_L_CTR', 'EGWD_CTR'])
+            ->delete();
+
+        // Add the new ones
+        DB::table('controller_positions')
+            ->insert(
+                [
+                    [
+                        'callsign' => 'BLKDG_CTR',
+                        'frequency' => 133.32,
+                        'created_at' => Carbon::now(),
+                    ],
+                    [
+                        'callsign' => 'BLKDG_APP',
+                        'frequency' => 133.32,
+                        'created_at' => Carbon::now(),
+                    ],
+                    [
+                        'callsign' => 'HTSPR_CTR',
+                        'frequency' => 135.07,
+                        'created_at' => Carbon::now(),
+                    ],
+                    [
+                        'callsign' => 'HTSPR_APP',
+                        'frequency' => 135.07,
+                        'created_at' => Carbon::now(),
+                    ],
+                ]
+            );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Delete the new positions
+        DB::table('controller_positions')
+            ->whereIn('callsign', ['BLKDG_CTR', 'BLKDG_APP', 'HTSPR_CTR', 'HTSPR_APP'])
+            ->delete();
+
+        // Add the old ones
+        DB::table('controller_positions')
+            ->insert(
+                [
+                    [
+                        'callsign' => 'EGVV_L_CTR',
+                        'frequency' => 127.45,
+                        'created_at' => Carbon::now(),
+                    ],
+                    [
+                        'callsign' => 'EGWD_CTR',
+                        'frequency' => 135.27,
+                        'created_at' => Carbon::now(),
+                    ],
+                ]
+            );
+    }
+}
