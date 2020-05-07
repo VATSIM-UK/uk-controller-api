680a1c816873824468710cedef0a8623d362bc2d
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-07 18:02:20 +0100

    Add Navaids As Dependency

---
+++ database/migrations/2020_05_07_170020_add_navaids_dependency.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AddNavaidsDependency extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('dependencies')
+            ->insert(
+                [
+                    [
+                        'key' => 'DEPENDENCY_NAVAIDS',
+                        'uri' => 'navaids/dependency',
+                        'local_file' => 'navaids.json',
+                        'created_at' => Carbon::now(),
+                    ],
+                ]
+            );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('dependencies')
+            ->where('key', 'DEPENDENCY_NAVAIDS')
+            ->delete();
+    }
+}
