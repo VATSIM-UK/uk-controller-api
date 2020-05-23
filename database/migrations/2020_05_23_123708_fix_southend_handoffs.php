6e95e6eb065ea2b6ea126fa2f200a69064ac5a01
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-23 13:39:27 +0100

    Fix #154: Update EGMC PDR Handoffs

---
+++ database/migrations/2020_05_23_123708_fix_southend_handoffs.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Services\HandoffService;
+use Illuminate\Database\Migrations\Migration;
+
+class FixSouthendHandoffs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        HandoffService::removeFromHandoffOrder('EGMC_PDR_CLACTON', 'EGMC_APP');
+        HandoffService::removeFromHandoffOrder('EGMC_PDR_EVNAS', 'EGMC_APP');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        HandoffService::insertIntoOrderBefore('EGMC_PDR_CLACTON', 'EGMC_APP', 'THAMES_APP');
+        HandoffService::insertIntoOrderBefore('EGMC_PDR_EVNAS', 'EGMC_APP', 'THAMES_APP');
+    }
+}
