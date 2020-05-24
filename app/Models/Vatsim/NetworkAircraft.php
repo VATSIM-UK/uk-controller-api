f38bff501a398ffe58467a25b922b3b572b118e5
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-24 21:00:17 +0100

    Model For Aircraft

--- app/Models/Vatsim/NetworkAircraft.php
+++ app/Models/Vatsim/NetworkAircraft.php
@@ -1,10 +1,32 @@
 <?php
 
-
 namespace App\Models\Vatsim;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
 
-class NetworkAircraft
+class NetworkAircraft extends Model
 {
+    protected $fillable = [
+        'callsign',
+        'latitude',
+        'longitude',
+        'altitude',
+        'groundspeed',
+        'planned_aircraft',
+        'planned_depairport',
+        'planned_destairport',
+        'planned_altitude',
+        'transponder',
+        'planned_flighttype',
+        'planned_route',
+    ];
 
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function (NetworkAircraft $aircraft) {
+            $aircraft->setUpdatedAt(Carbon::now());
+        });
+    }
 }
