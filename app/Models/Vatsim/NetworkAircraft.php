41ce69acc1997047eba6c2fb3fd0f14b5f9e3beb
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-24 20:59:56 +0100

    Add Network Aircraft Table

---
+++ app/Models/Vatsim/NetworkAircraft.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Models\Vatsim;
+
+
+class NetworkAircraft
+{
+
+}
---
+++ database/migrations/2020_05_24_193401_add_network_aircraft_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNetworkAircraftTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('network_aircraft', function (Blueprint $table) {
+            $table->string('callsign')->comment('The aircrafts callsign');
+            $table->string('latitude')->comment('The aircrafts latitude');
+            $table->string('longitude')->comment('The aircrafts longitude');
+            $table->integer('altitude')->comment('The aircrafts current altitude');
+            $table->unsignedInteger('groundspeed')->comment('The aircrafts current ground speed');
+            $table->string('planned_aircraft')->nullable()->comment('The aircraft type');
+            $table->string('planned_depairport')->nullable()->comment('The departure airport');
+            $table->string('planned_destairport')->nullable()->comment('The destination airport');
+            $table->string('planned_altitude')->nullable()->comment('The filed cruise altitude');
+            $table->string('transponder')->comment('The aircrafts current squawk code');
+            $table->string('planned_flighttype')->nullable()->comment('The aircrafts flight rules');
+            $table->text('planned_route')->nullable()->comment('The aircrafts planned route');
+            $table->timestamps();
+
+            $table->primary('callsign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('network_aircraft');
+    }
+}
