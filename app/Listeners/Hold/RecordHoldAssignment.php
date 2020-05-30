7483456e19d25b1fee416d0e0366afacc7af789e
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-30 14:07:23 +0100

    Table For Hold Assignment History

---
+++ app/Listeners/Hold/RecordHoldAssignment.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Listeners\Hold;
+
+
+class RecordHoldAssignment
+{
+
+}
---
+++ database/migrations/2020_05_30_130125_record_hold_assignment.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RecordHoldAssignment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hold_assignment_history', function (Blueprint $blueprint) {
+            $blueprint->id();
+            $blueprint->string('callsign')->comment('The callsign of the aircraft');
+            $blueprint->unsignedBigInteger('navaid_id')->nullable()->comment('The navaid the aircraft was assigned to hold at');
+            $blueprint->unsignedInteger('assigned_by')->comment('The user who made the assignment');
+            $blueprint->timestamp('assigned_at')->comment('The time the assignment was made');
+
+            $blueprint->foreign('navaid_id')->references('id')->on('navaids')->onDelete('cascade');
+            $blueprint->foreign('assigned_by')->references('id')->on('user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hold_assignment_history');
+    }
+}
