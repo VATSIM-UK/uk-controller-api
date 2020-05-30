948b693d8603e5e2f537603c07058e42987d953e
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-05-30 15:37:16 +0100

    Fix Vatsim Callsign Rule

--- app/Rules/VatsimCallsign.php
+++ app/Rules/VatsimCallsign.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Validation\Rule;
 
 class VatsimCallsign implements Rule
 {
-    const CALLSIGN_REGEX = '[A-Za-z0-9\-_]{1,10}';
+    const CALLSIGN_REGEX = '/^[A-Za-z0-9\-_]{1,10}$/';
 
     /**
      * Determine if the validation rule passes.
--- tests/app/Rules/VatsimCallsignTest.php
+++ tests/app/Rules/VatsimCallsignTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\Rules;
+namespace App\Rules;
 
 use App\BaseUnitTestCase;
 
