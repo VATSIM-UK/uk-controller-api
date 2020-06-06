e66f387fba10bc4ea6c498c3d6fbe69defab82d4
Andy Ford <TheHungryPredator@hotmail.co.uk>
2020-06-06 14:49:28 +0100

    Remove Hold Caching

--- app/Services/HoldService.php
+++ app/Services/HoldService.php
@@ -12,8 +12,6 @@ use Illuminate\Support\Facades\Log;
 
 class HoldService
 {
-    const CACHE_KEY = 'hold-data';
-
     /**
      * Returns the current holds in a format
      * that may be converted to a JSON array.
@@ -22,10 +20,6 @@ class HoldService
      */
     public function getHolds(): array
     {
-        if (Cache::has(self::CACHE_KEY)) {
-            return Cache::get(self::CACHE_KEY);
-        }
-
         $data = Hold::with('restrictions', 'navaid')->get()->toArray();
         foreach ($data as $key => $hold) {
             foreach ($hold['restrictions'] as $restrictionKey => $restriction) {
@@ -37,21 +31,7 @@ class HoldService
             unset($data[$key]['navaid_id'], $data[$key]['navaid']);
         }
 
-        Cache::forever(self::CACHE_KEY, $data);
         return $data;
-    }
-
-    /**
-     * Clear the hold cache
-     */
-    public function clearCache()
-    {
-        if (!Cache::forget(self::CACHE_KEY)) {
-            Log::warning('Hold cache clear failed');
-            return;
-        }
-
-        Log::info('Hold cache cleared');
     }
 
     /**
