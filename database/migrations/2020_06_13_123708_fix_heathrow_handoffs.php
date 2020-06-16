17c1acd9b28b9fccce5d74a7f82dbc433f3758a5
Andy Ford <AndyTWF@users.noreply.github.com>
2020-06-16 21:11:41 +0100

    161 heathrow handoffs (#164)
    * Service Method For Setting Handoffs
    
    * Fix #161: Heathrow Handoffs
    
    * Fix Patch Name
    
    * Fix Model
    
    * Fix Again

---
+++ database/migrations/2020_06_13_123708_fix_heathrow_handoffs.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Services\DependencyService;
+use App\Services\HandoffService;
+use Illuminate\Database\Migrations\Migration;
+
+class FixHeathrowHandoffs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        HandoffService::setHandoffForSid('EGLL', 'GOGSI2G', 'EGLL_SID_SOUTH_WEST');
+        HandoffService::setHandoffForSid('EGLL', 'GOGSI2F', 'EGLL_SID_SOUTH_WEST');
+        HandoffService::setHandoffForSid('EGLL', 'GASGU2J', 'EGLL_SID_SOUTH_WEST');
+        HandoffService::setHandoffForSid('EGLL', 'GASGU2K', 'EGLL_SID_SOUTH_WEST');
+        DependencyService::touchDependencyByKey('DEPENDENCY_SID_HANDOFF');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Nothing to do here
+    }
+}
