48100e90e89e7b540955c8dbf867e4df57364b91
Andy Ford <AndyTWF@users.noreply.github.com>
2020-06-30 19:12:01 +0100

    Enforced Primary Keys (#169)
    * Update Travis
    
    * Custom Passport Migrations
    
    * User Tables
    
    * Fix User Table
    
    * Fix HoldProfileHolds
    
    * Fix Other Migrations
    
    * Style
    
    * Match Passport Migrations To Original Names
    
    That way, we never run the risk of re-running in production during this
    upgrade or if we revert to the default Passport versions.
    
    * Fix Patches

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        /*
+         * Disable Passports migrations. We need to do this because the current production host runs
+         * on a managed database server on which primary keys are enforced. Laravel currently does not allow
+         * the creation of string primary keys in this environment as the queries are run in two parts. This is
+         * currently a wontfix on Laravel, so we have to write our own migrations to circumvent.
+         */
+        Passport::ignoreMigrations();
     }
     /**
      * Bootstrap any application services.
