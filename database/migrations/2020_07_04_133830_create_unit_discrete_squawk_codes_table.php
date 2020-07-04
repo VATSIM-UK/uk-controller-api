70201b1edb1a29c6006fa614f10e9235500c0d7f
Andy Ford <andyford993@gmail.com>
2020-07-04 14:39:30 +0100

    Add Table For Discrete Unit Squawks

---
+++ database/migrations/2020_07_04_133830_create_unit_discrete_squawk_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitDiscreteSquawkCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unit_discrete_squawk_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('unit')->comment('The unit to which the code applies');
+            $table->string('first', 4)->comment('The first squawk in the range');
+            $table->string('last', 4)->comment('The last squawk in the range');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unit_discrete_squawk_codes');
+    }
+}
