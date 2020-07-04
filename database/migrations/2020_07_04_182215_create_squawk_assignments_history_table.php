99eaa8324f6cb2437da4cbb0abce3053e38fc4dc
Andy Ford <andyford993@gmail.com>
2020-07-04 19:25:17 +0100

    Add Table For Squawk Assignment History

---
+++ database/migrations/2020_07_04_182215_create_squawk_assignments_history_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSquawkAssignmentsHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('squawk_assignments_history', function (Blueprint $table) {
+            $table->id();
+            $table->string('callsign')->comment('The callsign to which the squawk was assigned');
+            $table->string('code')->comment('The code that was assigned');
+            $table->enum('type', ['CCAMS', 'ORCAM', 'PAIR', 'UNIT'])->comment('The type of allocation');
+            $table->unsignedInteger('user_id')->nullable()->comment('The user that allocated the squawk');
+            $table->timestamp('allocated_at')->comment('The time the squawk was allocated');
+
+            $table->foreign('user_id')->references('id')->on('user')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('squawk_assignments_history');
+    }
+}
