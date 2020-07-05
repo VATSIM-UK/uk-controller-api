d03e4a11961ba66ec99fb4f92964c461c8ca2f28
Andy Ford <andyford993@gmail.com>
2020-07-05 17:19:59 +0100

    Add Delete And Fetch Methods

--- app/Allocator/Squawk/SquawkAllocatorInterface.php
+++ app/Allocator/Squawk/SquawkAllocatorInterface.php
@@ -4,7 +4,6 @@ namespace App\Allocator\Squawk;
 
 interface SquawkAllocatorInterface
 {
-
     /**
      * Allocates a squawk, given the aircrafts callsign and any other
      * pertinent details about its flight.
@@ -14,4 +13,20 @@ interface SquawkAllocatorInterface
      * @return SquawkAssignmentInterface|null
      */
     public function allocate(string $callsign, array $details): ?SquawkAssignmentInterface;
+
+    /**
+     * Deletes the allocation for a given callsign.
+     *
+     * @param string $callsign
+     * @return mixed
+     */
+    public function delete(string $callsign): void;
+
+    /**
+     * Fetches the allocation for a given callsign.
+     *
+     * @param string $callsign
+     * @return mixed
+     */
+    public function fetch(string $callsign): ?SquawkAssignmentInterface;
 }
