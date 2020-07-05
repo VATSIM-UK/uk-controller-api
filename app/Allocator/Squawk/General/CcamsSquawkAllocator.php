e5e20765610bc9d7ad394e2297a464ffa09cc3a1
Andy Ford <andyford993@gmail.com>
2020-07-05 17:20:21 +0100

    Create Ccams Squawk Allocator

---
+++ app/Allocator/Squawk/General/CcamsSquawkAllocator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Allocator\Squawk\General;
+
+use App\Allocator\Squawk\SquawkAllocatorInterface;
+use App\Allocator\Squawk\SquawkAssignmentInterface;
+use App\Models\Squawk\Ccams\CcamsSquawkAssignment;
+use App\Models\Squawk\Ccams\CcamsSquawkRange;
+use App\Models\Vatsim\NetworkAircraft;
+
+class CcamsSquawkAllocator implements SquawkAllocatorInterface
+{
+    public function allocate(string $callsign, array $details): ?SquawkAssignmentInterface
+    {
+        $assignment = null;
+        CcamsSquawkRange::all()->shuffle()->each(function (CcamsSquawkRange $range) use (&$assignment, $callsign) {
+            $allSquawks = $range->getAllSquawksInRange();
+            $possibleSquawks = $allSquawks->diff(
+                CcamsSquawkAssignment::whereIn('code', $allSquawks)->pluck('code')->all()
+            );
+
+            if ($possibleSquawks->isEmpty()) {
+                return true;
+            }
+
+            NetworkAircraft::firstOrCreate(
+                [
+                    'callsign' => $callsign
+                ]
+            );
+
+            $assignment = CcamsSquawkAssignment::create(
+                [
+                    'callsign' => $callsign,
+                    'code' => $possibleSquawks->first()
+                ]
+            );
+            return false;
+        });
+
+        return $assignment;
+    }
+
+    public function delete(string $callsign): void
+    {
+        CcamsSquawkAssignment::find($callsign)->delete();
+    }
+
+    public function fetch(string $callsign): ?SquawkAssignmentInterface
+    {
+        return CcamsSquawkAssignment::find($callsign);
+    }
+}
--- tests/app/Allocator/Squawk/General/CcamsSquawkAllocatorTest.php
+++ tests/app/Allocator/Squawk/General/CcamsSquawkAllocatorTest.php
@@ -1,10 +1,124 @@
 <?php
 
-
 namespace App\Allocator\Squawk\General;
 
+use App\BaseFunctionalTestCase;
+use App\Models\Squawk\Ccams\CcamsSquawkAssignment;
+use App\Models\Squawk\Ccams\CcamsSquawkRange;
+use App\Models\Vatsim\NetworkAircraft;
+use Carbon\Carbon;
 
-class CcamsSquawkAllocatorTest
+class CcamsSquawkAllocatorTest extends BaseFunctionalTestCase
 {
+    /**
+     * @var CcamsSquawkAllocator
+     */
+    private $allocator;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->allocator = new CcamsSquawkAllocator();
+    }
+
+    public function testItAllocatesFirstFreeSquawkInRange()
+    {
+        CcamsSquawkRange::create(
+            [
+                'first' => '7201',
+                'last' => '7210',
+            ]
+        );
+
+        NetworkAircraft::insert(
+            [
+                [
+                    'callsign' => 'VIR25F',
+                ],
+                [
+                    'callsign' => 'BAW92A',
+                ],
+            ]
+        );
+
+        CcamsSquawkAssignment::insert(
+            [
+                [
+                    'callsign' => 'VIR25F',
+                    'code' => '7201',
+                ],
+                [
+                    'callsign' => 'BAW92A',
+                    'code' => '7202',
+                ],
+            ]
+        );
+
+        $this->assertEquals('7203', $this->allocator->allocate('BMI11A', [])->getCode());
+        $this->assertDatabaseHas(
+            'ccams_squawk_assignments',
+            [
+                'callsign' => 'BMI11A',
+                'code' => '7203',
+                'created_at' => Carbon::now(),
+            ]
+        );
+    }
+
+    public function testItReturnsNullIfAllocationFails()
+    {
+        $this->assertNull($this->allocator->allocate('BAW123', []));
+    }
+
+    public function testItReturnsNullIfAllocationNotFound()
+    {
+        $this->assertNull($this->allocator->fetch('MMMMM'));
+    }
+
+    public function testItReturnsAllocationIfExists()
+    {
+        NetworkAircraft::create(
+            [
+                'callsign' => 'VIR25F',
+                'created_at' => Carbon::now(),
+            ],
+        );
+
+        CcamsSquawkAssignment::create(
+            [
+                'callsign' => 'VIR25F',
+                'code' => '0001',
+                'created_at' => Carbon::now(),
+            ],
+        );
+        $expected = CcamsSquawkAssignment::find('VIR25F');
+
+        $this->assertEquals($expected, $this->allocator->fetch('VIR25F'));
+    }
+
+    public function testItDeletesAllocations()
+    {
+        NetworkAircraft::create(
+            [
+                'callsign' => 'VIR25F',
+                'created_at' => Carbon::now(),
+            ],
+        );
+
+        $expected = CcamsSquawkAssignment::create(
+            [
+                'callsign' => 'VIR25F',
+                'code' => '0001',
+                'created_at' => Carbon::now(),
+            ],
+        );
+
+        $this->allocator->delete('VIR25F');
+        $this->assertDatabaseMissing(
+            'ccams_squawk_assignments',
+            [
+                'callsign' => 'VIR25F'
+            ]
+        );
+    }
 }
