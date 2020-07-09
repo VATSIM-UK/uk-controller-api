1ab71c564825cb1d984a04da6275328a2497ceac
Andy Ford <andyford993@gmail.com>
2020-07-09 17:41:01 +0100

    More Models

--- app/Models/FlightInformationRegion/FlightInformationRegion.php
+++ app/Models/FlightInformationRegion/FlightInformationRegion.php
@@ -1,10 +1,18 @@
 <?php
 
-
 namespace App\Models\FlightInformationRegion;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class FlightInformationRegion
+class FlightInformationRegion extends Model
 {
+    protected $fillable = [
+        'identification_code',
+    ];
 
+    public function boundaries(): HasMany
+    {
+        return $this->hasMany(FlightInformationRegionBoundary::class);
+    }
 }
