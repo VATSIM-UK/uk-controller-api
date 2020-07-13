6189e853d417e2974a842187b5ffe3a52e94393f
Andy Ford <andyford993@gmail.com>
2020-07-13 20:02:55 +0100

    Test Migration

--- database/migrations/2019_02_02_214826_create_hold_table.php
+++ database/migrations/2019_02_02_214826_create_hold_table.php
@@ -14,7 +14,7 @@ class CreateHoldTable extends Migration
     public function up()
     {
         Schema::create('hold', function (Blueprint $table) {
-            $table->unsignedInteger('id')->primary();
+            $table->increments('id');
             $table->string('fix')->comment('The holding fix');
             $table->smallInteger('inbound_heading')->comment('The heading');
             $table->mediumInteger('minimum_altitude')->comment('The minimum altitude allowed in the hold');
