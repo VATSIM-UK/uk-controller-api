a00ec16a3683992f7bebdc5d4c905a42c1d00ca7
Andy Ford <andyford993@gmail.com>
2020-07-14 19:12:21 +0100

    Add Web Route

---
+++ resources/views/web.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>UK Controller Api</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="ukcp-web-app"></div>
+
+<script src="{{ secure_asset('js/index.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-return [
+use Illuminate\Support\Facades\Route;
 
-];
+Route::get('/{path?}', 'WebInterface');
