6c40580a9e82b409b435a53377045751502b21bb
Andy Ford <andyford993@gmail.com>
2020-07-18 14:52:48 +0100

    Install Browser History

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,7 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import { browserHistory } from 'react-router';
 import Home from './components/Home';
 import Faq from './components/Faq';
 import Header from './components/Header';
@@ -12,7 +13,7 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Router basename="/web">
+      <Router basename="/web" history={browserHistory}>
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
