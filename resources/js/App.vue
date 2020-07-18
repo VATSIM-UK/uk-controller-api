f326ce3ae9c546d3a7564cfd6a7eefa88efd4aa5
Andy Ford <andyford993@gmail.com>
2020-07-18 20:13:29 +0100

    Properly Install Consilio

---
+++ resources/js/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <vuk-heading
+      type="heading-1"
+      class="text-blue-dark"
+    >
+      Hello world!
+    </vuk-heading>
+    <vuk-logo width="400px" variant="black-blue" />
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">
+            Example Component
+          </div>
+
+          <div class="card-body">
+            I'm an example component.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'App',
+};
+</script>
--- resources/js/index.ts
+++ resources/js/index.ts
@@ -1,15 +1,11 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
+import Vue from 'vue';
+import Consilio from '@vatsim-uk/consilio';
+import App from './App.vue';
+import '@vatsim-uk/consilio/dist/consilio.css';
 
-require('./bootstrap');
+Vue.use(Consilio);
+Vue.config.productionTip = false;
 
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-require('./app');
+new Vue({
+  render: (h) => h(App),
+}).$mount(document.getElementById('ukcp-web-app'));
