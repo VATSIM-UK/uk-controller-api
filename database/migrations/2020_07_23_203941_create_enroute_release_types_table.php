a5e7a4e26570842331664f97ab36aa066255f045
Andy Ford <andyford993@gmail.com>
2020-07-23 21:41:53 +0100

    Add Release Types Table

---
+++ database/migrations/2020_07_23_203941_create_enroute_release_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnrouteReleaseTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enroute_release_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag_string')->comment('What to display in the TAG for this kind of release');
+            $table->string('description')->comment('Description of the release to be shown in the plugin');
+            $table->timestamps();
+
+            $table->unique('key');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enroute_release_types');
+    }
+}
