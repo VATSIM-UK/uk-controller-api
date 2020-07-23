d730dcdd446118a627df2b21b8079b618f591ba6
Andy Ford <andyford993@gmail.com>
2020-07-23 21:52:47 +0100

    Table For Enroute Release History

---
+++ database/migrations/2020_07_23_204751_create_enroute_release_history_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnrouteReleaseHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enroute_release_history', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('enroute_release_type_id')->comment('The type of release');
+            $table->string('callsign')->comment('Which aircraft was the release for');
+            $table->string('initiating_controller')->comment('Who initiated the release');
+            $table->string('receiving_controller')->comment('Who received the release');
+            $table->unsignedInteger('user_id')->comment('Which user initiated the release');
+            $table->timestamp('released_at');
+
+            $table->foreign('user_id')->references('id')->on('user')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enroute_release_history');
+    }
+}
