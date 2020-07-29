d500cdc2b79f2feb916cde6c29ab2ec7fa8844f9
Andy Ford <andyford993@gmail.com>
2020-07-29 21:11:32 +0100

    Make Everything Clearer

--- app/Listeners/Squawk/ReserveInFirProximity.php
+++ app/Listeners/Squawk/ReserveInFirProximity.php
@@ -4,6 +4,7 @@ namespace App\Listeners\Squawk;
 
 use App\Events\NetworkAircraftUpdatedEvent;
 use App\Models\Squawk\Reserved\NonAssignableSquawkCode;
+use App\Models\Vatsim\NetworkAircraft;
 use App\Services\LocationService;
 use App\Services\SquawkService;
 use Carbon\Carbon;
@@ -14,6 +15,7 @@ use Location\Distance\Haversine;
 class ReserveInFirProximity
 {
     const MIN_DISTANCE = 650.0;
+    const MAX_GROUND_RESERVATION_SPEED = 45;
     const CONSISTENT_SQUAWK_MINUTES = 2;
 
     /**
@@ -56,23 +58,13 @@ class ReserveInFirProximity
                 continue;
             }
 
-            /*
-             * The aircraft is either:
-             * - Squawking its assigned code,
-             * - Squawking one of the reserved codes, so probably hasn't set its code yet
-             * - Only recently changed code, so let it go
-             */
-            if (
-                (($currentAssignment = $this->squawkService->getAssignedSquawk(
-                    $aircraft->callsign
-                )) && $aircraft->transponder == $currentAssignment->getCode()) ||
-                $aircraft->transponder_last_updated_at > Carbon::now()->subMinutes(self::CONSISTENT_SQUAWK_MINUTES) ||
-                NonAssignableSquawkCode::all()->contains($aircraft->transponder)
-            ) {
+            // We're close enough to try the reservation, check if we should reserve the squawk
+            if ($this->shouldNotReserveAssignment($aircraft)) {
                 break;
             }
 
-            // If the aircraft is close enough, lets try to reserve the squawk
+            // Try to reserve the squawk
+            $currentAssignment = $this->squawkService->getAssignedSquawk($aircraft->callsign);
             $newAssignment = $this->squawkService->reserveSquawkForAircraft($aircraft->callsign);
             if ($currentAssignment && $newAssignment) {
                 Log::info(
@@ -106,5 +98,32 @@ class ReserveInFirProximity
         }
 
         return true;
+    }
+
+    private function shouldNotReserveAssignment(NetworkAircraft $aircraft): bool
+    {
+        return $this->isSquawkingAssignedCode($aircraft) ||
+            $this->transponderRecentlyUpdated($aircraft) ||
+            $this->aircraftIsSquawkingNonAssignableCodeOnTheGround($aircraft) ||
+            $aircraft->squawkingMayday() ||
+            $aircraft->squawkingBannedSquawk() ||
+            $aircraft->squawkingRadioFailure();
+    }
+
+    private function isSquawkingAssignedCode(NetworkAircraft $aircraft): bool
+    {
+        return ($assignment = $this->squawkService->getAssignedSquawk($aircraft->callsign))
+            && $assignment->getCode() === $aircraft->squawk;
+    }
+
+    private function transponderRecentlyUpdated(NetworkAircraft $aircraft): bool
+    {
+        return $aircraft->transponder_last_updated_at > Carbon::now()->subMinutes(self::CONSISTENT_SQUAWK_MINUTES);
+    }
+
+    private function aircraftIsSquawkingNonAssignableCodeOnTheGround(NetworkAircraft $aircraft): bool
+    {
+        return NonAssignableSquawkCode::where('code', $aircraft->squawk)->exists() &&
+            $aircraft->groundspeed < self::MAX_GROUND_RESERVATION_SPEED;
     }
 }
