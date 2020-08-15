f43e3f0d16730a4cd718c1ef99e8948fb2ca87d5
Andy Ford <andyford993@gmail.com>
2020-08-15 16:49:13 +0100

    Add Stands Table

---
+++ database/migrations/2020_08_15_154608_create_stands_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stands', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('airfield_id')->comment('The airfield that the stand is at');
+            $table->unsignedInteger('identifier')->comment('The stand identifier');
+            $table->string('latitude')->comment('The stands latitude');
+            $table->string('longitude')->comment('The stands longitude');
+            $table->timestamps();
+
+            $table->foreign('airfield_id')->references('id')->on('airfield')->onDelete('cascade');
+            $table->unique('airfield_id', 'identifier');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stands');
+    }
+}
