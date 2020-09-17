dbd86ad38a88eeff86625d5b6df1c80160652190
Andy Ford <andyford993@gmail.com>
2020-09-17 18:14:58 +0100

    Remove Unused Imports

--- app/Console/Commands/SrdImport.php
+++ app/Console/Commands/SrdImport.php
@@ -3,8 +3,6 @@
 namespace App\Console\Commands;
 
 use App\Imports\Srd\SrdImport as ImportHelper;
-use App\Models\Srd\SrdNote;
-use App\Models\Srd\SrdRoute;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
