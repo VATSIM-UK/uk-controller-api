11a67032bede01c48d068e503bcb75b1e0fbfb0b
Andy Ford <andyford993@gmail.com>
2020-10-02 18:17:43 +0100

    Optimize Imports

--- database/migrations/2020_10_02_165818_stand_tidy_up.php
+++ database/migrations/2020_10_02_165818_stand_tidy_up.php
@@ -2,8 +2,6 @@
 
 use App\Services\StandService;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class StandTidyUp extends Migration
 {
