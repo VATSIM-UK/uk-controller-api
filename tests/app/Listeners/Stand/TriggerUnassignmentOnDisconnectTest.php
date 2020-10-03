fddcc64383a9587168442ade1ea347bb7be58fb5
Andy Ford <andyford993@gmail.com>
2020-10-03 09:50:48 +0100

    Fix #275: Stand Unassignment Spam

--- tests/app/Listeners/Stand/TriggerUnassignmentOnDisconnectTest.php
+++ tests/app/Listeners/Stand/TriggerUnassignmentOnDisconnectTest.php
@@ -5,7 +5,9 @@ namespace App\Listeners\Stand;
 use App\BaseFunctionalTestCase;
 use App\Events\NetworkAircraftDisconnectedEvent;
 use App\Events\StandUnassignedEvent;
+use App\Models\Stand\StandAssignment;
 use App\Models\Vatsim\NetworkAircraft;
+use App\Services\NetworkDataService;
 
 class TriggerUnassignmentOnDisconnectTest extends BaseFunctionalTestCase
 {
@@ -20,17 +22,40 @@ class TriggerUnassignmentOnDisconnectTest extends BaseFunctionalTestCase
         $this->listener = $this->app->make(TriggerUnassignmentOnDisconnect::class);
     }
 
-    public function testItFiresEvents()
+    public function testItFiresEventIfStandAssignmentExists()
     {
+        $this->addStandAssignment('BAW123', 1);
         $this->expectsEvents(StandUnassignedEvent::class);
-        $this->listener->handle(new NetworkAircraftDisconnectedEvent(new NetworkAircraft(['callsign' => 'BAW123'])));
+        $this->assertTrue(
+            $this->listener->handle(new NetworkAircraftDisconnectedEvent(NetworkAircraft::find('BAW123')))
+        );
     }
 
-    public function testItContinuesPropagation()
+    public function testItDeletesStandAssignments()
     {
+        $this->addStandAssignment('BAW123', 1);
         $this->expectsEvents([]);
+        $this->listener->handle(new NetworkAircraftDisconnectedEvent(NetworkAircraft::find('BAW123')));
+
+        $this->assertNull(StandAssignment::find('BAW123'));
+    }
+
+    public function testDoesntFireEventIfNoAssignment()
+    {
+        $this->doesntExpectEvents(StandUnassignedEvent::class);
         $this->assertTrue(
             $this->listener->handle(new NetworkAircraftDisconnectedEvent(new NetworkAircraft(['callsign' => 'BAW123'])))
+        );
+    }
+
+    private function addStandAssignment(string $callsign, int $standId): StandAssignment
+    {
+        NetworkDataService::firstOrCreateNetworkAircraft($callsign);
+        return StandAssignment::create(
+            [
+                'callsign' => $callsign,
+                'stand_id' => $standId,
+            ]
         );
     }
 }
