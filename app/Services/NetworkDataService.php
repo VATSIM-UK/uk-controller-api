3e282c2069dd843d20bdfc14e73ace8eb2b5e3bc
Andy Ford <andyford993@gmail.com>
2020-10-03 09:55:57 +0100

    Add Comment For Clarity

--- app/Services/NetworkDataService.php
+++ app/Services/NetworkDataService.php
@@ -48,6 +48,11 @@ class NetworkDataService
 
     /**
      * If any aircraft has passed the timeout window, remove it from the list.
+     *
+     * NOTE: Events should always fire before final deletion because the listeners
+     * will use the aircraft data to mark things such as squawk assignments as deleted
+     * and send further events. As a last resort calling delete here will delete any
+     * foreign key references left over.
      */
     private function handleTimeouts(): void
     {
