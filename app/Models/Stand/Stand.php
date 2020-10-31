559d1c10c3dae0ec6212b38e1da140c0ee67e9c3
Andy Ford <andyford993@gmail.com>
2020-10-31 19:53:09 +0000

    Add Custom Attribute and Cast To Model

--- app/Models/Stand/Stand.php
+++ app/Models/Stand/Stand.php
@@ -5,6 +5,7 @@ namespace App\Models\Stand;
 use App\Models\Airfield\Airfield;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Location\Coordinate;
 
 class Stand extends Model
 {
@@ -15,8 +16,18 @@ class Stand extends Model
         'longitude',
     ];
 
+    protected $casts = [
+        'latitude' => 'double',
+        'longitude' => 'double',
+    ];
+
     public function airfield(): BelongsTo
     {
         return $this->belongsTo(Airfield::class);
+    }
+
+    public function getCoordinateAttribute()
+    {
+        return new Coordinate($this->latitude, $this->longitude);
     }
 }
