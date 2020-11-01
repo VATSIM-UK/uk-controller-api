1b9be68d970be05d175cc24d840b1784b7135726
Andy Ford <andyford993@gmail.com>
2020-11-01 15:02:41 +0000

    Add Airlines Table

---
+++ database/migrations/2020_11_01_145740_create_airlines_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAirlinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('airlines', function (Blueprint $table) {
+            $table->id();
+            $table->string('icao_code')->comment('The airlines ICAO code');
+            $table->string('name')->comment('The name of the airline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('airlines');
+    }
+}
