62553516d95341f12659d1d75687686ed35decda
Andy Ford <andyford993@gmail.com>
2020-11-01 15:36:44 +0000

    Add Interface For Allocating Stands

---
+++ app/Allocator/Stand/StandAllocatorInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Allocator\Stand;
+
+use App\Models\Stand\Stand;
+use App\Models\Vatsim\NetworkAircraft;
+
+interface StandAllocatorInterface
+{
+    /**
+     * If called, should allocate a stand to an aircraft and return it. If an allocation
+     * is not possible, should return null.
+     *
+     * @param NetworkAircraft $aircraft
+     * @return Stand|null
+     */
+    public function allocate(NetworkAircraft $aircraft): ?Stand;
+}
