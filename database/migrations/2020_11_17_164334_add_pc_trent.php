bfb36b1b06cbc3febab84feae04d3d26a52f5f6b
Andy Ford <andyford993@gmail.com>
2020-11-17 16:57:08 +0000

    Add PC Trent For 2020/13

---
+++ database/migrations/2020_11_17_164334_add_pc_trent.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Services\DependencyService;
+use App\Services\HandoffService;
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddPcTrent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('controller_positions')
+            ->insert(
+                [
+                    'callsign' => 'MAN_T_CTR',
+                    'frequency' => '119.520',
+                    'created_at' => Carbon::now(),
+                ]
+            );
+        HandoffService::insertIntoOrderBefore('EGNX_SID_NORTH_27', 'MAN_T_CTR', 'MAN_S_CTR');
+        DependencyService::touchDependencyByKey('DEPENDENCY_CONTROLLER_POSITIONS');
+        DependencyService::touchDependencyByKey('DEPENDENCY_HANDOFF');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        HandoffService::removePositionFromAllHandoffs('MAN_T_CTR');
+        DB::table('controller_positions')->where('callsign', 'MAN_T_CTR')->delete();
+        DependencyService::touchDependencyByKey('DEPENDENCY_CONTROLLER_POSITIONS');
+        DependencyService::touchDependencyByKey('DEPENDENCY_HANDOFF');
+    }
+}
