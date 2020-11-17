a18549fbb70537cdb0d0cdbebe9b1c53b2b85784
Andy Ford <andyford993@gmail.com>
2020-11-17 17:05:48 +0000

    Remove Unused Includes

--- database/migrations/2020_11_17_164334_add_pc_trent.php
+++ database/migrations/2020_11_17_164334_add_pc_trent.php
@@ -4,9 +4,7 @@ use App\Services\DependencyService;
 use App\Services\HandoffService;
 use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class AddPcTrent extends Migration
 {
