a5d09224bf629a38631c306469af0482b6ff1471
Andy Ford <andyford993@gmail.com>
2020-12-05 15:27:46 +0000

    Add From To To Airline Stands

---
+++ database/migrations/2020_12_05_152205_add_not_before_to_airline_stand_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNotBeforeToAirlineStandTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('airline_stand', function (Blueprint $table) {
+            $table->time('from')
+                ->after('destination')
+                ->nullable()
+                ->comment('The earliest time in the day the stand can be allocated');
+            $table->time('to')
+                ->after('from')
+                ->nullable()
+                ->comment('The latest time in the day the stand can be allocated');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('airline_stand', function (Blueprint $table) {
+            $table->drop('from');
+            $table->drop('to');
+        });
+    }
+}
