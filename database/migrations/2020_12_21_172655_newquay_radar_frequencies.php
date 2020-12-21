684570d87502158764bf807226ab44aea9bcdc1b
Andy Ford <andyford993@gmail.com>
2020-12-21 17:29:57 +0000

    Fix #349: Newquay Radar Frequency

---
+++ database/migrations/2020_12_21_172655_newquay_radar_frequencies.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Services\DependencyService;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class NewquayRadarFrequencies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('controller_positions')
+            ->where('callsign', 'EGHQ_APP')
+            ->update(['frequency' => '133.400']);
+
+        DependencyService::touchDependencyByKey('DEPENDENCY_CONTROLLER_POSITIONS');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
