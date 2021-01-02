4aa22636d1fb4bb34e80fd60b0ebccc2caf13c92
Andy Ford <andyford993@gmail.com>
2021-01-02 19:01:05 +0000

    Add Dependency For Departure Intervals

---
+++ database/migrations/2021_01_02_190013_create_departure_sid_intervals_dependency.php
@@ -0,0 +1,37 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartureSidIntervalsDependency extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('dependencies')->insert(
+            [
+                'key' => 'DEPENDENCY_DEPARTURE_SID',
+                'uri' => 'departure/intervals/sid/dependency',
+                'local_file' => 'departure-sid-intervals.json',
+                'created_at' => Carbon::now(),
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('dependencies')->where('key', 'DEPENDENCY_DEPARTURE_SID')->delete();
+    }
+}
