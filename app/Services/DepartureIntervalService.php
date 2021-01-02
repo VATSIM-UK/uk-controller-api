24bd0e0880003e41d903d641bdebd89194bcbd44
Andy Ford <andyford993@gmail.com>
2021-01-02 14:48:57 +0000

    Add Function For Creating MDIs

--- app/Services/DepartureIntervalService.php
+++ app/Services/DepartureIntervalService.php
@@ -1,10 +1,51 @@
 <?php
 
-
 namespace App\Services;
 
+use App\Models\Departure\DepartureInterval;
+use App\Models\Departure\DepartureIntervalType;
+use App\Models\Sid;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 
 class DepartureIntervalService
 {
+    /**
+     * Create an MDI
+     */
+    public function createMinimumDepartureInterval(
+        int $interval,
+        string $airfield,
+        array $sids,
+        Carbon $expiresAt
+    ) : DepartureInterval {
+        $interval = $this->createDepartureInterval($interval, 'mdi', $expiresAt);
+        $this->addSidToDepartureInterval($interval, $airfield, $sids);
+        return $interval;
+    }
 
+    private function createDepartureInterval(int $interval, string $type, Carbon $expiresAt): DepartureInterval
+    {
+        return DepartureInterval::create(
+            [
+                'interval' => $interval,
+                'type_id' => DepartureIntervalType::where('key', $type)->first()->id,
+                'expires_at' => $expiresAt
+            ]
+        );
+    }
+
+    /**
+     * Associate sids with a newly created departure interval
+     */
+    private function addSidToDepartureInterval(DepartureInterval $interval, string $airfield, array $identifiers): void
+    {
+        $sids = Sid::whereHas('airfield', function (Builder $airfieldQuery) use ($airfield) {
+            $airfieldQuery->where('code', $airfield);
+        })
+            ->whereIn('identifier', $identifiers)
+            ->pluck('id');
+
+        $interval->sids()->sync($sids);
+    }
 }
--- tests/app/Services/DepartureIntervalServiceTest.php
+++ tests/app/Services/DepartureIntervalServiceTest.php
@@ -1,10 +1,57 @@
 <?php
 
-
 namespace App\Services;
 
+use App\BaseFunctionalTestCase;
+use Carbon\Carbon;
 
-class DepartureIntervalServiceTest
+class DepartureIntervalServiceTest extends BaseFunctionalTestCase
 {
+    /**
+     * @var DepartureIntervalService
+     */
+    private $service;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->service = $this->app->make(DepartureIntervalService::class);
+        Carbon::setTestNow(Carbon::now()->startOfSecond());
+    }
+
+    public function testItCreatesAMinimumDepartureIntervalWithSids()
+    {
+        $interval = $this->service->createMinimumDepartureInterval(
+            4,
+            'EGLL',
+            ['TEST1X', 'TEST1Y'],
+            Carbon::now()->addMinutes(10)
+        );
+
+        $this->assertDatabaseHas(
+            'departure_intervals',
+            [
+                'id' => $interval->id,
+                'type_id' => 1,
+                'interval' => 4,
+                'expires_at' => Carbon::now()->addMinutes(10),
+            ]
+        );
+
+        $this->assertDatabaseHas(
+            'departure_interval_sid',
+            [
+                'departure_interval_id' => $interval->id,
+                'sid_id' => 1,
+            ]
+        );
+
+        $this->assertDatabaseHas(
+            'departure_interval_sid',
+            [
+                'departure_interval_id' => $interval->id,
+                'sid_id' => 2,
+            ]
+        );
+    }
 }
