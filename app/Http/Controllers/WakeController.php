bdb076b51050a002c86128d45afae3241119b821
Andy Ford <andyford993@gmail.com>
2021-01-21 19:48:43 +0000

    Add Controller For Wake

--- app/Http/Controllers/WakeController.php
+++ app/Http/Controllers/WakeController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\WakeService;
+use Illuminate\Http\JsonResponse;
+
 class WakeController extends BaseController
 {
-    public function getWakeSchemesDependency()
-    {
+    private WakeService $wakeService;
 
+    public function __construct(WakeService $wakeService)
+    {
+        $this->wakeService = $wakeService;
+    }
+
+    public function getWakeSchemesDependency(): JsonResponse
+    {
+        return response()->json($this->wakeService->getWakeSchemesDependency());
     }
 }
