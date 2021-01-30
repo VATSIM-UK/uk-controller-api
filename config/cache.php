6637b53b4f0aaca6f1bad1a0818bd9d60e17dc4d
Andy Ford <andyford993@gmail.com>
2021-01-30 17:16:15 +0000

    More Redis Config

--- config/cache.php
+++ config/cache.php
@@ -9,7 +9,7 @@ return [
     | Default Cache Store
     |--------------------------------------------------------------------------
     |
-    | This option controls the default cache connection that gets used while
+    | This option controls the default cache connec\\tion that gets used while
     | using this caching library. This connection is used when another is
     | not explicitly specified when executing a given caching function.
     |
