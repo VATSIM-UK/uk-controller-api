ee644b0f600dd6428039f1b4159048198f4a7034
Andy Ford <andyford993@gmail.com>
2021-02-18 16:12:05 +0000

    Add Deemed Separated Holds

---
+++ database/migrations/2021_02_18_160623_create_deemed_separated_holds_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeemedSeparatedHoldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deemed_separated_holds', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('first_hold_id')->comment('The first hold in the pair');
+            $table->unsignedInteger('second_hold_id')->comment('The second hold in the pair');
+            $table->unique(['first_hold_id', 'second_hold_id'], 'deemed_separated_holds_pairs');
+
+            $table->foreign('first_hold_id')->references('id')->on('holds')->cascadeOnDelete();
+            $table->foreign('second_hold_id')->references('id')->on('holds')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deemed_separated_holds');
+    }
+}
---
+++ database/migrations/2021_02_18_160904_add_deemed_separated_holds.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDeemedSeparatedHolds extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $willo = DB::table('holds')
+            ->join('navaids', 'holds.navaid_id', '=', 'navaids.id')
+            ->where('navaids.identifier', 'WILLO')
+            ->first()
+            ->id;
+
+        $timba = DB::table('holds')
+            ->join('navaids', 'holds.navaid_id', '=', 'navaids.id')
+            ->where('navaids.identifier', 'TIMBA')
+            ->first()
+            ->id;
+
+        DB::table('deemed_separated_holds')->insert(
+            [
+                [
+                    'first_hold_id' => $willo,
+                    'second_hold_id' => $timba,
+                ],
+                [
+                    'first_hold_id' => $timba,
+                    'second_hold_id' => $willo,
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
