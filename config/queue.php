caaa6073bf9f471f1d5c28aaf6c671aa16fe4d44
Andy Ford <andyford993@gmail.com>
2021-02-19 15:10:08 +0000

    Run Jobs After Commit

--- config/queue.php
+++ config/queue.php
@@ -54,6 +54,7 @@ return [
             'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => 90,
             'block_for' => null,
+            'after_commit' => true,
         ],
     ],
     /*
