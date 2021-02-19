5256a154867a2c0dec6a15be33dcca58f531d015
Andy Ford <andyford993@gmail.com>
2021-02-19 15:13:22 +0000

    Fix More Smells

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -8,16 +8,6 @@ use Laravel\Horizon\HorizonApplicationServiceProvider;
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
-    }
-
-    /**
      * Register the Horizon gate.
      *
      * This gate determines who can access Horizon in non-local environments.
