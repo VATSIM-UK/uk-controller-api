948e61178bfe47573bff5cbe1a9f3660651373ea
Andy Ford <andyford993@gmail.com>
2021-03-23 20:26:13 +0000

    Table For Storing Metars

---
+++ database/migrations/2021_03_23_202453_create_metar_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMetarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('metar', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('airfield_id')->unique()->comment('The airfield the METAR applies to');
+            $table->text('metar_string')->comment('The raw METAR text');
+            $table->timestamps();
+
+            $table->foreign('airfield_id')->references('id')->on('airfield')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('metar');
+    }
+}
