0c2b2c7401687fcb7431c0d846e465e6329db906
Andy Ford <andyford993@gmail.com>
2021-04-17 14:43:52 +0100

    Add Table Linking Controllers To Releases

---
+++ database/migrations/2021_04_17_133314_create_controller_position_departure_release_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateControllerPositionDepartureReleaseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('controller_position_departure_release', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('controller_position_id')
+                ->comment('The controller position the release has been requested from');
+            $table->unsignedBigInteger('departure_release_id')
+                ->comment('The release that is being requested');
+            $table->unsignedInteger('released_by')->nullable()->comment('Who gave the release');
+            $table->timestamp('released_at')->nullable()->comment('What time the release was given');
+            $table->timestamp('release_expires_at')->nullable()->comment('When the release expired');
+            $table->timestamp('rejected_at')->nullable()->comment('When the release was rejected');
+
+            $table->foreign('released_by')->references('id')->on('user')->cascadeOnDelete();
+            $table->unique(['controller_position_id', 'departure_release_id'], 'controller_release_unique');
+            $table->foreign('controller_position_id', 'departure_release_controller_position')
+                ->references('id')
+                ->on('controller_positions')
+                ->cascadeOnDelete();
+
+            $table->foreign('departure_release_id', 'departure_release_departure_release')
+                ->references('id')
+                ->on('departure_releases')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('controller_position_departure_release');
+    }
+}
