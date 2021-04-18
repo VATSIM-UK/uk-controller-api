e6a0c60488e3570069d863e65c72ebd2d1cb2490
Andy Ford <andyford993@gmail.com>
2021-04-18 17:48:09 +0100

    feat(holds): Remove stale hold assignments

--- app/Services/HoldService.php
+++ app/Services/HoldService.php
@@ -2,7 +2,12 @@
 
 namespace App\Services;
 
+use App\Events\HoldUnassignedEvent;
+use App\Models\Hold\AssignedHold;
 use App\Models\Hold\Hold;
+use Illuminate\Support\Facades\DB;
+use Location\Coordinate;
+use Location\Distance\Haversine;
 
 class HoldService
 {
@@ -33,5 +38,23 @@ class HoldService
         }
 
         return $data;
+    }
+
+    public function removeStaleAssignments(): void
+    {
+        DB::transaction(function () {
+            $assignmentsToRemove = AssignedHold::with('aircraft', 'navaid')->get()
+                ->filter(function (AssignedHold $hold) {
+                    return ($hold->aircraft->groundspeed === 0 && $hold->aircraft->altitude < 1000) ||
+                        $hold->aircraft->latLong->getDistance($hold->navaid->coordinate, new Haversine()) > 55000;
+                });
+
+            $assignmentsToRemove->each(function (AssignedHold $assignedHold) {
+               event(new HoldUnassignedEvent($assignedHold->callsign));
+            });
+
+            AssignedHold::whereIn('callsign', $assignmentsToRemove->pluck('callsign'))
+                ->delete();
+        });
     }
 }
