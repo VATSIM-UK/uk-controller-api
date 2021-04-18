de6bec4eba97829acca8accaa631bd68be14dee6
Andy Ford <andyford993@gmail.com>
2021-04-18 16:38:35 +0100

    Convert Coordinates To Sectorfile Format For Navaid Dependency

--- tests/app/Http/Controllers/NavaidControllerTest.php
+++ tests/app/Http/Controllers/NavaidControllerTest.php
@@ -12,19 +12,25 @@ class NavaidControllerTest extends BaseApiTestCase
             [
                 'id' => 1,
                 'identifier' => 'WILLO',
+                    'latitude' => 'N050.59.06.000',
+                    'longitude' => 'W000.11.30.000',
             ],
             [
                 'id' => 2,
                 'identifier' => 'TIMBA',
+                    'latitude' => 'N050.56.44.000',
+                    'longitude' => 'E000.15.42.000',
             ],
             [
                 'id' => 3,
                 'identifier' => 'MAY',
+                    'latitude' => 'N051.01.01.920',
+                    'longitude' => 'E000.06.58.000',
             ],
         ];
 
         $this->makeUnauthenticatedApiRequest(self::METHOD_GET, 'navaid/dependency')
             ->assertStatus(200)
-            ->assertJson($expected);
+            ->assertExactJson($expected);
     }
 }
