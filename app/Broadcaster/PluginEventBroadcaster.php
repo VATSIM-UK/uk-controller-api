b8f6463037539db4027e8d5872ea513255c2fe66
Andy Ford <andyford993@gmail.com>
2021-05-04 16:52:22 +0100

    feat(websockets): remove websockets package in favour of polling method
    Its causing memory issues on the server and the current method is more stable
    
    BREAKING CHANGE: Old websockets no longer work

--- app/Broadcaster/PluginEventBroadcaster.php
+++ app/Broadcaster/PluginEventBroadcaster.php
@@ -4,24 +4,15 @@ namespace App\Broadcaster;
 
 use App\Models\Plugin\PluginEvent;
 use Illuminate\Broadcasting\Broadcasters\Broadcaster;
-use Illuminate\Broadcasting\Broadcasters\PusherBroadcaster;
-use Pusher\Pusher;
 
 class PluginEventBroadcaster extends Broadcaster
 {
-    private PusherBroadcaster $pusherBroadcaster;
-
-    public function __construct(PusherBroadcaster $pusherBroadcaster)
-    {
-        $this->pusherBroadcaster = $pusherBroadcaster;
-    }
-
     /**
      * @codeCoverageIgnore
      */
     public function auth($request)
     {
-        return $this->pusherBroadcaster->auth($request);
+        return true;
     }
 
     /**
@@ -29,12 +20,11 @@ class PluginEventBroadcaster extends Broadcaster
      */
     public function validAuthenticationResponse($request, $result)
     {
-        return $this->validAuthenticationResponse($request, $request);
+        return true;
     }
 
     public function broadcast(array $channels, $event, array $payload = [])
     {
-        $this->pusherBroadcaster->broadcast($channels, $event, $payload);
         unset($payload['socket']);
         PluginEvent::create(
             [
