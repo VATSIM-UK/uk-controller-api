c190fd744cc90075907fd1d8a1b74b8244dddb43
Andy Ford <andyford993@gmail.com>
2021-05-23 14:24:34 +0100

    Add Departure Release Indexes

---
+++ database/migrations/2021_05_23_132259_add_departure_release_requests_indexes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDepartureReleaseRequestsIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('departure_release_requests', function (Blueprint $table) {
+            $table->index('expires_at');
+            $table->index('release_expires_at');
+            $table->index('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
