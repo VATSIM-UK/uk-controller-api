543b7fdda3f68e61f0fe85c3acb22b8c8b369f89
Andy Ford <andyford993@gmail.com>
2021-12-19 21:01:44 +0000

    Tidy

--- app/Models/Hold/Hold.php
+++ app/Models/Hold/Hold.php
@@ -1,11 +1,8 @@
 <?php
 namespace App\Models\Hold;
 
-use App\Models\Hold\Hold;
 use App\Models\Navigation\Navaid;
-use App\Models\Hold\HoldRestriction;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
