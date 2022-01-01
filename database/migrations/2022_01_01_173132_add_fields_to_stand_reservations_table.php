1001ae01a7fb3e260d8f3d6eace3ff75f320d38e
Andy <andyford993@gmail.com>
2022-01-01 17:33:31 +0000

    Add flightplan columns to stand reservations

---
+++ database/migrations/2022_01_01_173132_add_fields_to_stand_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToStandReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stand_reservations', function (Blueprint $table) {
+            $table->string('origin', 4)->after('callsign')->comment('The origin airfield');
+            $table->string('destination', 4)->after('origin')->comment('The destination airfield');
+
+            $table->index('origin');
+            $table->index('destination');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stand_reservations', function (Blueprint $table) {
+            $table->dropColumn('origin');
+            $table->dropColumn('destination');
+        });
+    }
+}
