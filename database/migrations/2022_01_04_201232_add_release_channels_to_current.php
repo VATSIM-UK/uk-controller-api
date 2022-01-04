af497b93b913e1a4bc69d7f15f158e994a9130ed
Andy Ford <andyford993@gmail.com>
2022-01-04 20:17:18 +0000

    Fix patch

--- database/migrations/2022_01_04_201232_add_release_channels_to_current.php
+++ database/migrations/2022_01_04_201232_add_release_channels_to_current.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Version\PluginReleaseChannel;
+use App\Models\Version\Version;
 
 class AddReleaseChannelsToCurrent extends Migration
 {
