e6db2014f5dde6f3e829197a6641ae315a0acc1b
Andy Ford <andyford993@gmail.com>
2022-01-04 20:16:11 +0000

    fix(releases): Fix departure releases to work on 7.4 for now

--- app/Events/DepartureReleaseApprovedEvent.php
+++ app/Events/DepartureReleaseApprovedEvent.php
@@ -16,9 +16,10 @@ class DepartureReleaseApprovedEvent extends HighPriorityBroadcastEvent
 
     public function broadcastWith()
     {
+        $expiresAt = $this->approval->release_expires_at;
         return [
             'id' => $this->approval->id,
-            'expires_at' => $this->approval->release_expires_at?->toDateTimeString(),
+            'expires_at' => $expiresAt ? $expiresAt->toDateTimeString() : null,
             'released_at' => $this->approval->release_valid_from->toDateTimeString(),
             'remarks' => $this->approval->remarks,
         ];
