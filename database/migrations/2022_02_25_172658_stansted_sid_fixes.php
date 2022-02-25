1c88b1a7a532a044acd17fc827eb65bcf1dd3f7e
Andy Ford <andyford993@gmail.com>
2022-02-25 17:33:10 +0000

    fix(stansted): Sid identifiers didnt update

---
+++ database/migrations/2022_02_25_172658_stansted_sid_fixes.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Sid;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Migrations\Migration;
+
+class StanstedSidFixes extends Migration
+{
+    const CHANGES = [
+        'CLN4S' => 'CLN5S',
+        'CLN1E' => 'CLN2E',
+        'CLN8R' => 'CLN9R',
+        'DET1D' => 'DET2D',
+        'DET1S' => 'DET2S',
+        'DET1R' => 'DET2R',
+        'LAM2S' => 'LAM3S',
+        'LAM3R' => 'LAM4R',
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (self::CHANGES as $oldIdentifer => $newIdentifier) {
+            Sid::whereHas('runway.airfield', function (Builder $airfield) {
+                $airfield->where('code', 'EGSS');
+            })
+                ->where('identifier', $oldIdentifer)
+                ->firstOrFail()
+                ->update(['identifier' => $newIdentifier]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
