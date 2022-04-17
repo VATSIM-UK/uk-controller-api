2ea759e7517b365d2bbab8c237f25f62ac3db3d6
Andy Ford <andyford993@gmail.com>
2022-04-17 16:36:31 +0100

    Combinations and negations

---
+++ tests/app/Services/IntentionCode/Condition/AnyOfTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services\IntentionCode\Condition;
+
+use App\BaseUnitTestCase;
+use App\Services\IntentionCode\Builder\ConditionBuilder;
+
+class AnyOfTest extends BaseUnitTestCase
+{
+    private AnyOf $anyOf;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->anyOf = new AnyOf(
+            tap(
+                new ConditionBuilder(),
+                function (ConditionBuilder $conditionBuilder) {
+                    $conditionBuilder->maximumCruisingLevel(35000);
+                    $conditionBuilder->arrivalAirfields(['EGKK']);
+                }
+            )
+        );
+    }
+
+    public function testItReturnsArray()
+    {
+        $expected = [
+            'type' => 'any_of',
+            'conditions' => [
+                [
+                    'type' => 'maximum_cruising_level',
+                    'level' => 35000,
+                ],
+                [
+                    'type' => 'arrival_airfields',
+                    'airfields' => ['EGKK'],
+                ]
+            ]
+        ];
+
+        $this->assertEquals($expected, $this->anyOf->toArray());
+    }
+}
