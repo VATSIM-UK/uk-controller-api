728dce646fb2a22ed1da4cc39eb2aeaea6eae795
Andy Ford <andyford993@gmail.com>
2022-05-13 16:55:06 +0100

    Reposition stands

--- database/migrations/2022_05_11_214313_east_midlands_stands.php
+++ database/migrations/2022_05_11_214313_east_midlands_stands.php
@@ -18,28 +18,56 @@ class EastMidlandsStands extends Migration
             ['80', '82'],
         ],
         '82' => [
-            '524940.68N 0011900.05W',
+            '524940.60N 0011858.69W',
             'LM',
             ['81'],
         ],
         '83' => [
-            '524940.68N 0011900.05W',
+            '524940.85N 0011855.22W',
             'H',
+            [],
         ],
         '84' => [
-            '524940.68N 0011900.05W',
+            '524942.82N 0011854.79W',
             'LM',
             [],
         ],
         '85' => [
-            '524940.68N 0011900.05W',
+            '524943.65N 0011855.10W',
             'UM',
             ['86'],
         ],
         '86' => [
-            '524940.68N 0011900.05W',
+            '524944.09N 0011854.88W',
             'LM',
             ['85'],
+        ],
+    ];
+
+    const STANDS_TO_UPDATE = [
+        '20' => [
+            '524943.55N 0011944.91W',
+            ['21'],
+        ],
+        '21' => [
+            '524942.19N 0011945.84W',
+            ['20'],
+        ],
+        '22' => [
+            '524943.49N 0011948.22W',
+            [],
+        ],
+        '23' => [
+            '524942.10N 0011950.46W',
+            ['24'],
+        ],
+        '24' => [
+            '524942.41N 0011949.45W',
+            ['23'],
+        ],
+        '80' => [
+            '524940.74N 0011900.79W',
+            [],
         ],
     ];
 
@@ -61,11 +89,28 @@ class EastMidlandsStands extends Migration
             ->airfield('EGNX')
             ->delete();
 
+        // Re-position stands 20-24 and 80
+        foreach (self::STANDS_TO_UPDATE as $identifier => $details) {
+            $coordinateSplit = explode(' ', $details[0]);
+            $coordinate = SectorfileService::coordinateFromNats($coordinateSplit[0], $coordinateSplit[1]);
+
+            $stand = Stand::where('identifier', $identifier)
+                ->airfield('EGNX')
+                ->firstOrFail();
+
+            $stand->update(
+                [
+                    'latitude' => $coordinate->getLat(),
+                    'longitude' => $coordinate->getLng(),
+                ]
+            );
+            $stand->pairedStands()->sync(Stand::whereIn('identifier', $details[1])->airfield('EGNX')->pluck('id'));
+        }
+
         // Add stands 81-86
         $eastMidlands = Airfield::where('code', 'EGNX')->firstOrFail()->id;
         $eastApron = Terminal::where('key', 'EGNX_E')->firstOrFail()->id;
         $cargo = StandType::where('key', 'CARGO')->firstOrFail()->id;
-
 
         foreach (self::STANDS_TO_ADD as $identifier => $details) {
             $coordinateSplit = explode(' ', $details[0]);
