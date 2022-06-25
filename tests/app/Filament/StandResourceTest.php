ed5a4b8bc6b6956f5360998cdb55f0b85cb238c5
Andy Ford <andyford993@gmail.com>
2022-06-25 12:32:55 +0100

    Do it without pest

--- tests/app/Filament/StandResourceTest.php
+++ tests/app/Filament/StandResourceTest.php
@@ -1,23 +1,25 @@
 <?php
 
+namespace App\Filament;
+
 use App\BaseFilamentTestCase;
 use App\Filament\Resources\StandResource;
-
 use App\Models\User\User;
 
-use function Pest\Livewire\livewire;
+class StandResourceTest extends BaseFilamentTestCase
+{
+    public function testItIsForbiddenIfNotAdminUser()
+    {
+        $this->actingAs(User::factory()->create());
+        $this->get(StandResource::getUrl())
+            ->assertForbidden();
+    }
 
-it('is forbidden if not admin user ', function() {
-    /** @var BaseFilamentTestCase $this */
-    $this->actingAs(User::factory()->create());
-    $this->get(StandResource::getUrl())
-        ->assertForbidden();
-});
-
-it('renders the index page ', function() {
-    /** @var BaseFilamentTestCase $this */
-    $this->get(StandResource::getUrl())
-        ->assertSuccessful()
-        ->assertSeeText('EGLL')
-        ->assertSeeText('New stand');
-});
+    public function testItRendersTheIndexPage()
+    {
+        $this->get(StandResource::getUrl())
+            ->assertSuccessful()
+            ->assertSeeText('EGLL')
+            ->assertSeeText('New stand');
+    }
+}
