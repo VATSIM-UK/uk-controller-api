e136b3644e313f5c4efa8046d1101c330a7f9ec1
Andy Ford <AndyTWF@users.noreply.github.com>
2022-07-12 15:38:15 +0100

    Redirect old route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('version/latest', function () {
+    return redirect('api/version/latest);
+});
+
 require __DIR__.'/auth.php';
