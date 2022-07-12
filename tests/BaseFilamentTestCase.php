57ef29f7aff160732d3059f276a9ac9d4fd99314
Andy Ford <andyford993@gmail.com>
2022-07-12 19:10:45 +0100

    test: fix tests

--- tests/BaseFilamentTestCase.php
+++ tests/BaseFilamentTestCase.php
@@ -2,13 +2,13 @@
 
 namespace App;
 
-use App\Models\User\Admin;
+use App\Models\User\User;
 
 abstract class BaseFilamentTestCase extends BaseFunctionalTestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-        $this->actingAs(Admin::findOrFail(self::ACTIVE_USER_CID), 'web_admin');
+        $this->actingAs(User::findOrFail(self::ACTIVE_USER_CID), 'web');
     }
 }
