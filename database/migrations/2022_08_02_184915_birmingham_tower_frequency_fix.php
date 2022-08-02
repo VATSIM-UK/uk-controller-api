7304d744766199cc9933b53ab1f2ca7f2e9acb9a
Andy Ford <andyford993@gmail.com>
2022-08-02 19:57:28 +0100

    style: styleci fixes

--- database/migrations/2022_08_02_184915_birmingham_tower_frequency_fix.php
+++ database/migrations/2022_08_02_184915_birmingham_tower_frequency_fix.php
@@ -3,8 +3,7 @@
 use App\Models\Controller\ControllerPosition;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
