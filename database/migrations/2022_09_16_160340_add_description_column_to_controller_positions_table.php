b7676ac12b3d741852d08dfebca864059f973327
Andy Ford <andyford993@gmail.com>
2022-09-16 18:22:08 +0100

    feat: controller position descriptions

---
+++ database/migrations/2022_09_16_160340_add_description_column_to_controller_positions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('controller_positions', function (Blueprint $table) {
+            $table->string('description')
+                ->nullable()
+                ->after('callsign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('controller_positions', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+};
