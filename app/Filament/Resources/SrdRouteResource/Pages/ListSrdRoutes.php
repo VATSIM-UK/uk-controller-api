846327b8ab824475609897e1f8e95688bd691d31
Andy Ford <andyford993@gmail.com>
2022-09-30 15:06:56 +0100

    feat: srd version on routes page

--- app/Filament/Resources/SrdRouteResource/Pages/ListSrdRoutes.php
+++ app/Filament/Resources/SrdRouteResource/Pages/ListSrdRoutes.php
@@ -3,10 +3,15 @@
 namespace App\Filament\Resources\SrdRouteResource\Pages;
 
 use App\Filament\Resources\SrdRouteResource;
-use Filament\Pages\Actions;
+use App\Services\AiracService;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSrdRoutes extends ListRecords
 {
     protected static string $resource = SrdRouteResource::class;
+
+    public function getTitle(): string
+    {
+        return sprintf('SRD Routes: AIRAC %s', AiracService::getCurrentAirac());
+    }
 }
