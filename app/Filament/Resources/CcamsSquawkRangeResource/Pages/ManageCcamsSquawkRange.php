c200065c1ff69daef6d69a43ee133952a4de97ea
Andy Ford <andyford993@gmail.com>
2022-10-21 17:21:31 +0100

    fix: limit records on the management class

--- app/Filament/Resources/CcamsSquawkRangeResource/Pages/ManageCcamsSquawkRange.php
+++ app/Filament/Resources/CcamsSquawkRangeResource/Pages/ManageCcamsSquawkRange.php
@@ -3,9 +3,11 @@
 namespace App\Filament\Resources\CcamsSquawkRangeResource\Pages;
 
 use App\Filament\Resources\CcamsSquawkRangeResource;
+use App\Filament\Resources\Pages\LimitsTableRecordListingOptions;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageCcamsSquawkRange extends ManageRecords
 {
+    use LimitsTableRecordListingOptions;
     protected static string $resource = CcamsSquawkRangeResource::class;
 }
