4f92e528911359cfa6ae44fd1349fba5f1d9483f
Andy <andyford993@gmail.com>
2023-05-04 18:12:56 +0100

    Add adjustment column to the ASR table

---
+++ database/migrations/2023_05_04_165358_add_offset_to_altimeter_setting_regions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('altimeter_setting_region', function (Blueprint $table) {
+            $table->tinyInteger('adjustment')
+                ->after('name')
+                ->default(-1)
+                ->comment('The lowest QNH in the region is adjusted by this amount to produce the RPS');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('altimeter_setting_region', function (Blueprint $table) {
+            $table->dropColumn('adjustment');
+        });
+    }
+};
