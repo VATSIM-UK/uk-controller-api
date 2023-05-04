6636baeffc59e38f4f552d73eda9c6ea58719696
Andy <andyford993@gmail.com>
2023-05-04 18:34:23 +0100

    data: update adjustment for tmas
    resolves #1310

---
+++ database/migrations/2023_05_04_172704_change_adjustments_for_tmas.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use App\Models\AltimeterSettingRegions\AltimeterSettingRegion;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        AltimeterSettingRegion::whereIn('key', ['ASR_LONDON', 'ASR_MANCHESTER', 'ASR_SCOTTISH'])
+            ->update(['adjustment' => 0]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        AltimeterSettingRegion::whereIn('key', ['ASR_LONDON', 'ASR_MANCHESTER', 'ASR_SCOTTISH'])
+            ->update(['adjustment' => -1]);
+    }
+};
