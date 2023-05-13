0a88bcb8b6eb35c172d8d84e8c077a8431eb3429
Andy Ford <andyford993@gmail.com>
2023-05-13 16:29:48 +0100

    fix: query escaping

--- app/Allocator/Stand/AbstractArrivalStandAllocator.php
+++ app/Allocator/Stand/AbstractArrivalStandAllocator.php
@@ -61,7 +61,16 @@ abstract class AbstractArrivalStandAllocator implements ArrivalStandAllocatorInt
                 // Prefer stands that haven't been requested by someone else
                 $join->on('stands.id', '=', 'other_stand_requests.stand_id')
                     ->on('other_stand_requests.user_id', '<>', $join->raw($aircraft->cid))
-                    ->on('other_stand_requests.requested_time', '>', $join->raw(Carbon::now()));
+                    ->on(
+                        'other_stand_requests.requested_time',
+                        '>',
+                        $join->raw(
+                            sprintf(
+                                '\'%s\'',
+                                Carbon::now()
+                            )
+                        )
+                    );
             })
             ->orderByRaw('other_stand_requests.id IS NULL')
             ->inRandomOrder();
