d7466c8d99f225824278817f500213848963dfc7
Andy Ford <andyford993@gmail.com>
2023-08-29 17:12:13 +0000

    style: sonarcloud duplication

--- app/Policies/ReadOnlyWithRolePolicy.php
+++ app/Policies/ReadOnlyWithRolePolicy.php
@@ -8,7 +8,7 @@ use App\Models\User\User;
 /**
  * Policy that allows read only access only to users with a role.
  */
-class ReadOnlyWithRolePolicy
+class ReadOnlyWithRolePolicy extends ReadOnlyPolicy
 {
     use HandlesAuthorization;
     use ChecksUserRoles;
@@ -21,75 +21,5 @@ class ReadOnlyWithRolePolicy
     public function viewAny(?User $user): bool
     {
         return $this->userHasAnyRole($user);
-    }
-
-    public function attach(): bool
-    {
-        return false;
-    }
-
-    public function detach(): bool
-    {
-        return false;
-    }
-
-    public function update(): bool
-    {
-        return false;
-    }
-
-    public function create(): bool
-    {
-        return false;
-    }
-
-    public function delete(): bool
-    {
-        return false;
-    }
-
-    public function restore(): bool
-    {
-        return false;
-    }
-
-    public function forceDelete(): bool
-    {
-        return false;
-    }
-
-    public function detachAny(): bool
-    {
-        return false;
-    }
-
-    public function dissociate(): bool
-    {
-        return false;
-    }
-
-    public function dissociateAny(): bool
-    {
-        return false;
-    }
-
-    public function replicate(): bool
-    {
-        return false;
-    }
-
-    public function restoreAny(): bool
-    {
-        return false;
-    }
-
-    public function deleteAny(): bool
-    {
-        return false;
-    }
-
-    public function forceDeleteAny(): bool
-    {
-        return false;
     }
 }
