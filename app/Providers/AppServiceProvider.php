e0888c026901499b6eda98c493082c1ee8f07184
Andy Ford <andy.ford@ably.com>
2024-04-20 15:15:57 +0100

    deps: remove old passport workaround
    We had to work around because of laravel not handling primary keys created in certain ways. This was fixed.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,22 +21,6 @@ use Livewire\Livewire;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        /*
-         * Disable Passports migrations. We need to do this because the current production host runs
-         * on a managed database server on which primary keys are enforced. Laravel currently does not allow
-         * the creation of string primary keys in this environment as the queries are run in two parts. This is
-         * currently a wontfix on Laravel, so we have to write our own migrations to circumvent.
-         */
-        Passport::ignoreMigrations();
-    }
-
-    /**
      * Bootstrap any application services.
      *
      * @return void
