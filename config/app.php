39849189c6e5c3e7efee013e6832a9ba59941eee
Andy Ford <andy.ford@ably.com>
2024-04-21 19:39:22 +0100

    register bugsnag facade

--- config/app.php
+++ config/app.php
@@ -207,6 +207,7 @@ return [
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
         'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
+        'Bugsnag' => Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
         'Bus' => Illuminate\Support\Facades\Bus::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
         'Config' => Illuminate\Support\Facades\Config::class,
