b35fdcdf66d4f8dd91e90fb54178bcc1d6bba5c3
Andy Ford <andy.ford@ably.com>
2024-04-21 20:04:13 +0100

    test: remove redundant test-case things

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -11,24 +11,4 @@ abstract class BaseTestCase extends TestCase
     public const ACTIVE_USER_CID = 1203533;
     public const BANNED_USER_CID = 1203534;
     public const DISABLED_USER_CID = 1203535;
-
-    /**
-     * Creates the application.
-     *
-     * @return Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-        $app->make(Kernel::class)->bootstrap();
-        return $app;
-    }
-
-    /**
-     * Returns the root of the tests folder.
-     */
-    public function getTestRoot()
-    {
-        return base_path() . '/tests';
-    }
 }
