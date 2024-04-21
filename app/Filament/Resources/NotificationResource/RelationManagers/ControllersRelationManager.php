e771a4e548ee651ec2753693ded312df8bd46f67
Andy Ford <andy.ford@ably.com>
2024-04-21 19:14:30 +0100

    style: sonarcloud

--- app/Filament/Resources/NotificationResource/RelationManagers/ControllersRelationManager.php
+++ app/Filament/Resources/NotificationResource/RelationManagers/ControllersRelationManager.php
@@ -81,6 +81,7 @@ class ControllersRelationManager extends RelationManager
                     ->action(function (AttachAction $action) {
                         $action->process(
                             function () {
+                                return;
                             }
                         );
                         $action->success();
