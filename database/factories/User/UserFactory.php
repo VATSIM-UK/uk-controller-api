78ff53eb57621e4ec4275006ecf2884037c49b23
Andy Ford <andy.ford@ably.com>
2024-04-28 17:09:29 +0100

    test: use global faker in user factory

--- database/factories/User/UserFactory.php
+++ database/factories/User/UserFactory.php
@@ -14,7 +14,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'id' => $this->faker->unique()->numberBetween(800000, 1900000),
+            'id' => fake()->unique()->numberBetween(800000, 1900000),
         ];
     }
 }
