439472d5ad253d37110b20740f811191c49b276c
Patrick Winters <19wintersp@gmail.com>
2025-05-14 18:06:27 +0100

    docs: add note about output format

--- app/Http/Controllers/SmrAreaController.php
+++ app/Http/Controllers/SmrAreaController.php
@@ -10,6 +10,8 @@ class SmrAreaController extends Controller
     public function getFormattedSmrAreas(): Response
     {
         return response(
+            // backwards-compatible format for vSMR: sline-format coordinates on
+            // each line, with polygons separated by at least one blank line
             SmrArea::active()->pluck("coordinates")->join("\n\n"),
             200,
             ["Content-Type" => "text/plain"],
