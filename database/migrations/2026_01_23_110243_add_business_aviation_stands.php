0b0f226e643c2055ddc197f70fc488e6387c9783
clc0609 <cobylc.121@gmail.com>
2026-01-23 11:27:42 +0000

    chore: add basic stand list with migration

---
+++ database/migrations/2026_01_23_110243_add_business_aviation_stands.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Stand\StandType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    private array $businessAviationStands = [
+        'EGLL' => ['501', '502', '503', '504', '505', '506'],
+        'EGKK' => ['230R', '230L', '231R', '231L', '232R', '232L', '233R', '233L', '234R', '234L','235R' '235L'],
+    ];
+
+    private int $businessTypeId = DB::table('stand_types')
+        ->where('key', StandType::TYPE_KEY_BUSINESS_AVIATION)
+        ->value('id');
+
+    public function up(): void
+    {
+        foreach ($this->businessAviationStands as $icao => $standIdentifiers) {
+            $airfieldId = DB::table('airfields')
+                ->where('code', $icao)
+                ->value('id');
+
+            DB::table('stands')
+                ->where('airfield_id', $airfieldId)
+                ->whereIn('identifier', $standIdentifiers)
+                ->update([
+                    'type_id' => $businessTypeId,
+                ]);
+        }
+    }
+
+    public function down(): void
+    {
+        foreach ($this->businessAviationStands as $icao => $standIdentifiers) {
+            $airfieldId = DB::table('airfields')
+                ->where('code', $icao)
+                ->value('id');
+
+            if (! $airfieldId) {
+                continue;
+            }
+
+            DB::table('stands')
+                ->where('airfield_id', $airfieldId)
+                ->whereIn('identifier', $standIdentifiers)
+                ->update([
+                    'type_id' => null,
+                ]);
+        }
+    }
+};
