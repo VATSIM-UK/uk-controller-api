84507f1696bc1435e8c40501fded41b443883769
Daniel 'MrAdder' Green <dgreen03@gmail.com>
2026-02-15 13:02:25 +0000

    Style issyue

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,3 @@ Route::middleware('guest')->group(function () {
 
 // Redirect to allow old plugin versions to update to the latest
 Route::get('version/latest', fn () => Redirect::to('api/version/latest'));
-
-
