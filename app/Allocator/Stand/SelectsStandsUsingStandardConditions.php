6e6e0a6dccedeeb5d2a60a4cc787f77b07ff57d5
Daniel 'MrAdder' Green <dgreen03@gmail.com>
2026-03-11 20:39:58 +0000

    Style CI I love you

--- app/Allocator/Stand/SelectsStandsUsingStandardConditions.php
+++ app/Allocator/Stand/SelectsStandsUsingStandardConditions.php
@@ -71,8 +71,7 @@ trait SelectsStandsUsingStandardConditions
                 $aircraft
             ),
             $this->orderByForStandsQuery($aircraft, $specificOrders, $includeAssignmentPriority, $isRanking)
-
-    );
+        );
     }
 
     /**
