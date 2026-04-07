311eeedfe2a0eed31f321efa6b405a2e4928f345
Kristián Kunc <kristian.kunc@gmail.com>
2026-04-07 23:30:10 +0200

    tailwind v4

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-// import react from '@vitejs/plugin-react';
-// import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/css/filament/admin/theme.css'
-        ]),
-        // react(),
-        // vue({
-        //     template: {
-        //         transformAssetUrls: {
-        //             base: null,
-        //             includeAbsolute: false,
-        //         },
-        //     },
-        // }),
+        tailwindcss(),
+        laravel({
+            input: ["resources/css/app.css", "resources/css/filament/admin/theme.css"],
+            refresh: true,
+        }),
     ],
 });
