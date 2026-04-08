a27f26f4c6528de63f4f985ca046661f11f06ac8
Daniel Green <dgreen03@gmail.com>
2026-04-08 18:56:48 +0100

    final formatting fix

--- database/migrations/2026_04_08_000100_add_vaa_role.php
+++ database/migrations/2026_04_08_000100_add_vaa_role.php
@@ -4,7 +4,7 @@ use App\Models\User\Role;
 use App\Models\User\RoleKeys;
 use Illuminate\Database\Migrations\Migration;
 
-return new class () extends Migration {
+return new class() extends Migration {
     public function up(): void
     {
         Role::create([
@@ -17,6 +17,4 @@ return new class () extends Migration {
     {
         Role::where('key', RoleKeys::VAA->value)->delete();
     }
-
-
 };
